703bb0cd9b6d07e08359cec8b6c7570a3bbb42e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 15:04:49 +0000

    Add package ansible-tower-mcp/1.2.4/ansible_tower_mcp-1.2.4-py3-none-any.whl

---
+++ packages/ansible-tower-mcp/ansible_tower_mcp-1.2.4-py3-none-any.whl/ansible_tower_mcp/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from ansible_tower_mcp.ansible_tower_api import Api
+from ansible_tower_mcp.ansible_tower_mcp import ansible_tower_mcp
+
+"""
+Ansible Tower MCP Server and API
+"""
+
+__all__ = ["Api", "ansible_tower_mcp"]
