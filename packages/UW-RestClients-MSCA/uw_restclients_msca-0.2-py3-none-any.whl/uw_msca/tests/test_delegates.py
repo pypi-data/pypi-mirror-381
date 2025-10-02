7f1aaca768b0de803728db5d55378b910021bb60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 04:04:07 +0000

    Add package UW-RestClients-MSCA/0.2/uw_restclients_msca-0.2-py3-none-any.whl

---
+++ packages/UW-RestClients-MSCA/uw_restclients_msca-0.2-py3-none-any.whl/uw_msca/tests/test_delegates.py
@@ -0,0 +1,23 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from unittest import TestCase
+from uw_msca.delegate import get_delegates, get_all_delegates
+from uw_msca.util import fdao_msca_override
+import mock
+
+
+@fdao_msca_override
+class GetDelegatesTest(TestCase):
+    def test_get_delegates(self):
+        delegates = get_delegates('javerage')
+        self.assertEqual(len(delegates), 2)
+
+        delegates = get_delegates('bill')
+        self.assertEqual(len(delegates), 1)
+
+    @mock.patch('uw_msca.delegate.get_external_resource')
+    def test_get_all_delegates(self, mock_method):
+        delegates = get_all_delegates()
+        mock_method.assert_called_with(
+            "https://host/blob/MbxAll-Report-date.csv")
