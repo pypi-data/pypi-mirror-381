0e214965e2bb92b95da954dbdd3d95c384b4d19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 16:04:43 +0000

    Add package bool-hybrid-array/9.6.9/bool_hybrid_array-9.6.9-py3-none-any.whl

---
+++ packages/bool-hybrid-array/bool_hybrid_array-9.6.9-py3-none-any.whl/bool_hybrid_array/__init__.py
@@ -0,0 +1,16 @@
+import sys
+from types import ModuleType
+from . import core
+import builtins
+__version__ = "9.6.9"
+public_objects = []
+for name in dir(core):
+    if not name.startswith("_"):
+        obj = getattr(core, name)
+        if isinstance(obj, (type, ModuleType)) or callable(obj):
+            public_objects.append(name)
+__all__ = public_objects + ["__version__","__builtins__","core"]
+globals().update({
+    name: getattr(core, name)
+    for name in public_objects
+})
