2f8227418a9b789373eebb2812d6f003288b1695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 14:05:11 +0000

    Add package altinoai/2.0.2/altinoai-2.0.2.tar.gz

---
+++ packages/altinoai/altinoai-2.0.2.tar.gz/altinoai-2.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='altinoai',
+    version='2.0.2',
+    packages=find_packages(),  # Automatically finds the package directories
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
