d9eb5832f77a4ce11c64ef60d633c4e96ddcc408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 14:03:59 +0000

    Add package WhisperDriver/0.1.7/WhisperDriver-0.1.7.tar.gz

---
+++ packages/WhisperDriver/WhisperDriver-0.1.7.tar.gz/WhisperDriver-0.1.7/WhisperDriver/Obj/__init__.py
@@ -0,0 +1,6 @@
+from .endpoints import WhisperTradesEndpoints
+from .bots import WhisperTradesBots
+from .variables import WhisperTradesVariables
+from .scheduler import WhisperTradesScheduler
+from .throttle import WhisperTradesThrottle
+from .via_ui import SeleniumDriver
---
+++ packages/WhisperDriver/WhisperDriver-0.1.7.tar.gz/WhisperDriver-0.1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+setup(
+    name="WhisperDriver",
+    version="0.1.7",
+    description="Automation and API library for WhisperTrades.com",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Paul Nobrega",
+    author_email="Paul@PaulNobrega.net",
+    url="https://github.com/PaulNobrega/WhisperDriver",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.10",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
