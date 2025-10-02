94ae71db8de162cac4f523e5bc6c588631da0112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 16:04:28 +0000

    Add package pyzk2/0.1/pyzk2-0.1.tar.gz

---
+++ packages/pyzk2/pyzk2-0.1.tar.gz/pyzk2-0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    description = fh.read()
+
+setup(
+    name='pyzk2',
+    version='0.1',
+    url='https://github.com/habibmhamed/pyzk2',
+    author='Habib Mhamadi',
+    author_email='habibmhamadi@gmail.com',
+    license='LICENSE.txt',
+    packages=find_packages(),
+    keywords=[
+        'zk',
+        'pyzk',
+        'pyzk2',
+        'zksoftware',
+        'attendance machine',
+        'fingerprint',
+        'biometrics',
+        'security'
+    ],
+    zip_safe=False,
+    long_description=description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+)
