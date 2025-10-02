991436e48c524624ab5b30959d6b5ce3b5ed0026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 14:04:57 +0000

    Add package boto3-stubs-full/1.40.43/boto3_stubs_full-1.40.43.tar.gz

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.40.43.tar.gz/boto3_stubs_full-1.40.43/mypy_boto3_resource_explorer_2/__init__.py
@@ -0,0 +1,61 @@
+"""
+Main interface for resource-explorer-2 service.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resource_explorer_2/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_resource_explorer_2 import (
+        Client,
+        ListIndexesForMembersPaginator,
+        ListIndexesPaginator,
+        ListManagedViewsPaginator,
+        ListResourcesPaginator,
+        ListSupportedResourceTypesPaginator,
+        ListViewsPaginator,
+        ResourceExplorerClient,
+        SearchPaginator,
+    )
+
+    session = Session()
+    client: ResourceExplorerClient = session.client("resource-explorer-2")
+
+    list_indexes_for_members_paginator: ListIndexesForMembersPaginator = client.get_paginator("list_indexes_for_members")
+    list_indexes_paginator: ListIndexesPaginator = client.get_paginator("list_indexes")
+    list_managed_views_paginator: ListManagedViewsPaginator = client.get_paginator("list_managed_views")
+    list_resources_paginator: ListResourcesPaginator = client.get_paginator("list_resources")
+    list_supported_resource_types_paginator: ListSupportedResourceTypesPaginator = client.get_paginator("list_supported_resource_types")
+    list_views_paginator: ListViewsPaginator = client.get_paginator("list_views")
+    search_paginator: SearchPaginator = client.get_paginator("search")
+    ```
+"""
+
+from .client import ResourceExplorerClient
+from .paginator import (
+    ListIndexesForMembersPaginator,
+    ListIndexesPaginator,
+    ListManagedViewsPaginator,
+    ListResourcesPaginator,
+    ListSupportedResourceTypesPaginator,
+    ListViewsPaginator,
+    SearchPaginator,
+)
+
+Client = ResourceExplorerClient
+
+
+__all__ = (
+    "Client",
+    "ListIndexesForMembersPaginator",
+    "ListIndexesPaginator",
+    "ListManagedViewsPaginator",
+    "ListResourcesPaginator",
+    "ListSupportedResourceTypesPaginator",
+    "ListViewsPaginator",
+    "ResourceExplorerClient",
+    "SearchPaginator",
+)
