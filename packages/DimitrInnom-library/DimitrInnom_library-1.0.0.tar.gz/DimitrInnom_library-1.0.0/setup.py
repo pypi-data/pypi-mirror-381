6d0fe19e4654411dbdb9d0cbcf02a9582cb4f3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 04:04:54 +0000

    Add package DimitrInnom-library/1.0.0/DimitrInnom_library-1.0.0.tar.gz

---
+++ packages/DimitrInnom-library/DimitrInnom_library-1.0.0.tar.gz/DimitrInnom_library-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DimitrInnom_library",
+    version="1.0.0",
+    author="Dimitri",
+    author_email="dimitri.kvateladze.1@iliauni.edu.ge",
+    description="A simple example package",
+    packages=find_packages(),        # Automatically find subpackages
+    python_requires=">=3.6"
+)
