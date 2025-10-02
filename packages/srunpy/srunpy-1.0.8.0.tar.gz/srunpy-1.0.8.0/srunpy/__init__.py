950ecb46da9dba50155f06e2651b492f69c37a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 16:05:26 +0000

    Add package srunpy/1.0.8.0/srunpy-1.0.8.0.tar.gz

---
+++ packages/srunpy/srunpy-1.0.8.0.tar.gz/srunpy-1.0.8.0/srunpy/__init__.py
@@ -0,0 +1,11 @@
+PROGRAM_VERSION = (1, 0, 8, 0)
+__version__ = '.'.join(map(str, PROGRAM_VERSION))
+
+from .html import WebRoot
+from .srun import Srun_Py as SrunClient
+import platform
+if platform.system() == 'Windows':
+    from .interface import MainWindow, TaskbarIcon, GUIBackend
+
+
+print('SrunClient version:', __version__)
