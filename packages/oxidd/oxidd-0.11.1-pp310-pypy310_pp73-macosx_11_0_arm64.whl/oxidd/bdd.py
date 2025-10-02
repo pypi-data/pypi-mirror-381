a36539cbb406c1e1ba3ae3a8a593d67d62452cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:04:20 +0000

    Add package oxidd/0.11.1/oxidd-0.11.1-pp310-pypy310_pp73-macosx_11_0_arm64.whl

---
+++ packages/oxidd/oxidd-0.11.1-pp310-pypy310_pp73-macosx_11_0_arm64.whl/oxidd/bdd.py
@@ -0,0 +1,5 @@
+"""Binary decision diagrams (BDDs, without complement edges)."""
+
+__all__ = ["BDDFunction", "BDDManager", "BDDSubstitution"]
+
+from ._oxidd import BDDFunction, BDDManager, BDDSubstitution
