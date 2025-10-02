b80e9fe8906d2a305431444b7e92f3e42e7025b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 17:05:11 +0000

    Add package chaeruldesktop/0.1.0/chaeruldesktop-0.1.0.tar.gz

---
+++ packages/chaeruldesktop/chaeruldesktop-0.1.0.tar.gz/chaeruldesktop-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chaeruldesktop",
+    version="0.1.0",
+    author="Chaerul",
+    description="Komponen GUI sederhana berbasis CustomTkinter untuk aplikasi desktop Python",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/chaerul24/chaeruldesktop",  # Ganti dengan URL GitHub kamu
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "customtkinter",
+        "pillow"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3.7',
+)
+
