9093434236d8e430042b64c861269d6ebfb66b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 04:04:09 +0000

    Add package dissect.fve/4.5.dev3/dissect_fve-4.5.dev3.tar.gz

---
+++ packages/dissect.fve/dissect_fve-4.5.dev3.tar.gz/dissect_fve-4.5.dev3/tests/test_luks_kdf.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+
+import pytest
+
+from dissect.fve.crypto import argon2
+from dissect.fve.luks.luks import derive_passphrase_key
+from dissect.fve.luks.metadata import Keyslot
+
+
+def test_luks_kdf_pbkdf2() -> None:
+    keyslot = Keyslot.from_dict(
+        {
+            "type": "luks2",
+            "key_size": 32,
+            "af": {"type": "luks1", "stripes": 4000, "hash": "sha256"},
+            "area": {"type": "raw", "offset": "32768", "size": "131072", "encryption": "aes-ecb", "key_size": 32},
+            "kdf": {
+                "type": "pbkdf2",
+                "hash": "sha256",
+                "iterations": 3426718,
+                "salt": "fmh2v7DaJ2D/tFkvvGB+mogBu3s+tUpDuKaf0vQyqIA=",
+            },
+        }
+    )
+
+    assert derive_passphrase_key(b"password", keyslot) == bytes.fromhex(
+        "05cff19a3cbc2d5612bdbcaee94db532b95b2cd33a997d1f2b30ffb166f302a6"
+    )
+
+
+@pytest.mark.skipif(not argon2.HAS_ARGON2, reason="Argon2 is not available, skipping")
+def test_luks_kdf_argon2i() -> None:
+    keyslot = Keyslot.from_dict(
+        {
+            "type": "luks2",
+            "key_size": 32,
+            "af": {"type": "luks1", "stripes": 4000, "hash": "sha256"},
+            "area": {"type": "raw", "offset": "32768", "size": "131072", "encryption": "aes-ecb", "key_size": 32},
+            "kdf": {
+                "type": "argon2i",
+                "time": 5,
+                "memory": 1048576,
+                "cpus": 4,
+                "salt": "fsv0tZWR6Q/WkidkWY6p0jiP1A+am8CH8h3D8gYEoYE=",
+            },
+        }
+    )
+
+    try:
+        assert derive_passphrase_key(b"password", keyslot) == bytes.fromhex(
+            "37f6085467d330749f59ea348491908a4faddabcb67523efc649419ecb52bd94"
+        )
+    except ValueError as e:
+        if "Hashing failed: out of memory" in str(e):
+            pytest.skip("Argon2 failed due to insufficient memory, skipping")
+        raise
+
+
+@pytest.mark.skipif(not argon2.HAS_ARGON2, reason="Argon2 is not available, skipping")
+def test_luks_kdf_argon2id() -> None:
+    keyslot = Keyslot.from_dict(
+        {
+            "type": "luks2",
+            "key_size": 32,
+            "af": {"type": "luks1", "stripes": 4000, "hash": "sha256"},
+            "area": {"type": "raw", "offset": "32768", "size": "131072", "encryption": "aes-ecb", "key_size": 32},
+            "kdf": {
+                "type": "argon2id",
+                "time": 5,
+                "memory": 1048576,
+                "cpus": 4,
+                "salt": "yssVZKHcjdSON4vM096WRyzZBWfz5Pf+a08cRzPdcZg=",
+            },
+        }
+    )
+
+    try:
+        assert derive_passphrase_key(b"password", keyslot) == bytes.fromhex(
+            "15b7b31551e6df90e6dbc58bb5dbc5f82efa8f36fdab9422f8a9bdfa72a8af85"
+        )
+    except ValueError as e:
+        if "Hashing failed: out of memory" in str(e):
+            pytest.skip("Argon2 failed due to insufficient memory, skipping")
+        raise
