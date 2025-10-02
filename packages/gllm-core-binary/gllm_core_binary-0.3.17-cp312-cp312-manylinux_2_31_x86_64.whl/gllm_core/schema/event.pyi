7880bacef66ffc62f44bf8266d4cf74b17224f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 16:04:41 +0000

    Add package gllm-core-binary/0.3.17/gllm_core_binary-0.3.17-cp312-cp312-manylinux_2_31_x86_64.whl

---
+++ packages/gllm-core-binary/gllm_core_binary-0.3.17-cp312-cp312-manylinux_2_31_x86_64.whl/gllm_core/schema/event.pyi
@@ -0,0 +1,31 @@
+from datetime import datetime
+from gllm_core.constants import EventLevel as EventLevel, EventType as EventType
+from pydantic import BaseModel
+from typing import Any
+
+class Event(BaseModel):
+    """A data class to store an event attributes.
+
+    Attributes:
+        value (str | dict[str, Any]): The value of the event.
+        level (EventLevel): The severity level of the event. Defined through the EventLevel constants.
+        type (EventType): The type of the event. Defined through the EventType constants.
+        timestamp (datetime): The timestamp of the event. If not provided, the current timestamp is used.
+        metadata (dict[str, Any]): The metadata of the event. Defaults to an empty dictionary.
+    """
+    value: str | dict[str, Any]
+    level: EventLevel
+    type: EventType
+    timestamp: datetime
+    metadata: dict[str, Any]
+    def serialize_level(self, level: EventLevel) -> str:
+        """Serializes an EventLevel object into its string representation.
+
+        This method serializes the given EventLevel object by returning its name as a string.
+
+        Args:
+            level (EventLevel): The EventLevel object to be serialized.
+
+        Returns:
+            str: The name of the EventLevel object.
+        """
