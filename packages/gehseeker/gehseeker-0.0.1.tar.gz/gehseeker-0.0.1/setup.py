9601f0392b557b5ce1c1c3f1556f87db1135e83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:05:00 +0000

    Add package gehseeker/0.0.1/gehseeker-0.0.1.tar.gz

---
+++ packages/gehseeker/gehseeker-0.0.1.tar.gz/gehseeker-0.0.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages, Extension
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Meinolf Sellmann",
+    author_email='info@insideopt.com',
+    python_requires='>=3.11.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows'
+    ],
+    description="InsideOpt Seeker Distribution for GEH on Windows",
+    install_requires=requirements,
+    long_description=readme, 
+    keywords='insideopt, seeker, optimization',
+    name='gehseeker',
+    test_suite='tests',
+    version='0.0.1',
+    packages=find_packages(include=['gehseeker', 'gehseeker.*', '*.so', '*.pyd', '*.dll']),
+    package_data={'gehseeker': ['*.so', 'gehseeker.py', 'bin/*', 'scripts/*', '*.pyd', '*.dll']},
+    zip_safe=False,
+)
