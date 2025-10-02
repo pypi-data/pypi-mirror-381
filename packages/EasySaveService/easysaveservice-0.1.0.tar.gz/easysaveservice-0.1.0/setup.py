073bd70893b816ba9ab4bfe1f675d8931658fc73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 16:04:36 +0000

    Add package EasySaveService/0.1.0/easysaveservice-0.1.0.tar.gz

---
+++ packages/EasySaveService/easysaveservice-0.1.0.tar.gz/easysaveservice-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EasySaveService',  
+    version='0.1.0', 
+    packages=find_packages(), 
+    install_requires=[ 
+        'requests',
+        'enum'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='David Comor',
+    author_email='david.comor@gmail.com',
+    description='EasySave service python library',
+    url='https://github.com/DavComo/EasySaveModule',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
