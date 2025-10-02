5ef072709077e20f8465003c0395ee8a33104601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 17:05:10 +0000

    Add package fasttext-community/0.10.8/fasttext_community-0.10.8-cp310-cp310-macosx_15_0_arm64.whl

---
+++ packages/fasttext-community/fasttext_community-0.10.8-cp310-cp310-macosx_15_0_arm64.whl/fasttext/tests/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .test_configurations import get_supervised_models
+from . import test_script
