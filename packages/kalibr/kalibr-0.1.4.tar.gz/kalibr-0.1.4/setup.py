ba2645c39710c5272ec9a8c25a058d7ca41d7917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 14:05:16 +0000

    Add package kalibr/0.1.4/kalibr-0.1.4.tar.gz

---
+++ packages/kalibr/kalibr-0.1.4.tar.gz/kalibr-0.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kalibr",  # package name on PyPI
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "requests"
+    ],
+    entry_points={
+        "console_scripts": [
+            "kalibr-demo=kalibr.__main__:main"
+        ]
+    },
+    author="Kalibr",
+    description="SDK for making SaaS agent-ready with 7 lines of code",
+    url="https://kalibr.systems",
+    python_requires=">=3.8",
+)
