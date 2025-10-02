75f0797dbe040d1e485ac1cf5ac07cf3a63556a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:04:43 +0000

    Add package eegdash/0.4.0.dev173498563/eegdash-0.4.0.dev173498563-py3-none-any.whl

---
+++ packages/eegdash/eegdash-0.4.0.dev173498563-py3-none-any.whl/eegdash/utils.py
@@ -0,0 +1,24 @@
+# Authors: The EEGDash contributors.
+# License: GNU General Public License
+# Copyright the EEGDash contributors.
+
+"""General utility functions for EEGDash.
+
+This module contains miscellaneous utility functions used across the EEGDash package,
+including MongoDB client initialization and configuration helpers.
+"""
+
+from mne.utils import get_config, set_config, use_log_level
+
+
+def _init_mongo_client():
+    with use_log_level("ERROR"):
+        if get_config("EEGDASH_DB_URI") is None:
+            set_config(
+                "EEGDASH_DB_URI",
+                "mongodb+srv://eegdash-user:mdzoMjQcHWTVnKDq@cluster0.vz35p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+                set_env=True,
+            )
+
+
+__all__ = ["_init_mongo_client"]
