62d4798662d54aae80380456f9da0172606c808e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 14:05:21 +0000

    Add package mdsasa-bolt/1.2.0/mdsasa_bolt-1.2.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/mdsasa-bolt/mdsasa_bolt-1.2.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/mdsasa_bolt/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2025 Maxwell J. Campbell
+"""MDSASA-Bolt: Solvent Accessible Surface Area Analysis for MD Trajectories."""
+
+from .analysis import SASAAnalysis
+
+__version__ = "0.1.0"
+__all__ = ["SASAAnalysis"]
