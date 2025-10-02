7cbd7872015f5d17ad0013f936f5fff708ad3102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 15:04:06 +0000

    Add package fp-cate/0.1.13/fp_cate-0.1.13-py3-none-any.whl

---
+++ packages/fp-cate/fp_cate-0.1.13-py3-none-any.whl/fp_cate/__init__.py
@@ -0,0 +1,20 @@
+from .utils import (
+    identity,
+    assert_and,
+    expr,
+    compose,
+    curry,
+    pipe,
+    dbg,
+    Filter,
+    State,
+)
+
+# fmt: off
+__all__ = [
+    # utils
+    "identity", "assert_and", "expr", "compose", "curry", "pipe",
+    "dbg",
+    "Filter", "State",
+]
+# fmt: on
