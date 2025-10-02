65cf482a45b8b4605d8b674fe85652e724da479a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:04:19 +0000

    Add package oxidd/0.11.1/oxidd-0.11.1-cp39-abi3-musllinux_1_2_x86_64.whl

---
+++ packages/oxidd/oxidd-0.11.1-cp39-abi3-musllinux_1_2_x86_64.whl/oxidd/util.py
@@ -0,0 +1,17 @@
+"""Primitives and utilities."""
+
+__all__ = [
+    "BooleanOperator",
+    "DDDMPFile",
+    "DDDMPVersion",
+    "DDMemoryError",
+    "DuplicateVarName",
+]
+
+from ._oxidd import (
+    BooleanOperator,
+    DDDMPFile,
+    DDDMPVersion,
+    DDMemoryError,
+    DuplicateVarName,
+)
