c3a93539c9e256e94d96d2a61893f17bb1d3f7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 16:04:40 +0000

    Add package yitorch/0.1.0/yitorch-0.1.0-py3-none-any.whl

---
+++ packages/yitorch/yitorch-0.1.0-py3-none-any.whl/yitorch/const.py
@@ -0,0 +1,8 @@
+__PKG__ = 'yitorch'
+__VERSION__ = '0.1.0'
+__ENV__ = '/etc/yitech/.env'
+
+DATE_FORMAT = "%Y-%m-%d"
+TIME_FORMAT = "%H:%M:%S"
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+PRESION_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
---
+++ packages/yitorch/yitorch-0.1.0-py3-none-any.whl/yitorch/enums.py
@@ -0,0 +1,1 @@
+from yitool.enums import YiEnum
