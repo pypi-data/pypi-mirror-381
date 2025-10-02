f4b2c0c68a15e61664dfc84ec8498fe137bc31cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 15:04:34 +0000

    Add package aisuite/0.1.12/aisuite-0.1.12.tar.gz

---
+++ packages/aisuite/aisuite-0.1.12.tar.gz/aisuite-0.1.12/aisuite/providers/openai_provider.py
@@ -0,0 +1,40 @@
+import openai
+import os
+from aisuite.provider import Provider, LLMError
+from aisuite.providers.message_converter import OpenAICompliantMessageConverter
+
+
+class OpenaiProvider(Provider):
+    def __init__(self, **config):
+        """
+        Initialize the OpenAI provider with the given configuration.
+        Pass the entire configuration dictionary to the OpenAI client constructor.
+        """
+        # Ensure API key is provided either in config or via environment variable
+        config.setdefault("api_key", os.getenv("OPENAI_API_KEY"))
+        if not config["api_key"]:
+            raise ValueError(
+                "OpenAI API key is missing. Please provide it in the config or set the OPENAI_API_KEY environment variable."
+            )
+
+        # NOTE: We could choose to remove above lines for api_key since OpenAI will automatically
+        # infer certain values from the environment variables.
+        # Eg: OPENAI_API_KEY, OPENAI_ORG_ID, OPENAI_PROJECT_ID, OPENAI_BASE_URL, etc.
+
+        # Pass the entire config to the OpenAI client constructor
+        self.client = openai.OpenAI(**config)
+        self.transformer = OpenAICompliantMessageConverter()
+
+    def chat_completions_create(self, model, messages, **kwargs):
+        # Any exception raised by OpenAI will be returned to the caller.
+        # Maybe we should catch them and raise a custom LLMError.
+        try:
+            transformed_messages = self.transformer.convert_request(messages)
+            response = self.client.chat.completions.create(
+                model=model,
+                messages=transformed_messages,
+                **kwargs,  # Pass any additional arguments to the OpenAI API
+            )
+            return response
+        except Exception as e:
+            raise LLMError(f"An error occurred: {e}")
