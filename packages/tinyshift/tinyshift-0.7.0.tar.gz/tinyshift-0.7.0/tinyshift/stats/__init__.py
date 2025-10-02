1a3ae49df52563c003ead7f5c708c71a67805ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 14:05:05 +0000

    Add package tinyshift/0.7.0/tinyshift-0.7.0.tar.gz

---
+++ packages/tinyshift/tinyshift-0.7.0.tar.gz/tinyshift-0.7.0/tinyshift/stats/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024-2025 Lucas Leão
+# tinyshift - A small toolbox for mlops
+# Licensed under the MIT License
+
+
+from .utils import chebyshev_guaranteed_percentage
+from .statistical_interval import StatisticalInterval
+from .bootstrap_bca import BootstrapBCA
