a6602b084c1ec64b84d4620915956328044e215d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 16:05:28 +0000

    Add package ewoksid31/0.7.0/ewoksid31-0.7.0.tar.gz

---
+++ packages/ewoksid31/ewoksid31-0.7.0.tar.gz/ewoksid31-0.7.0/src/ewoksid31/app/integrate/constants.py
@@ -0,0 +1,25 @@
+FILE_EXTENSIONS = ".h5", ".hdf5", ".hdf", ".nx", ".nxs", ".nx5", ".nexus"
+
+MONITOR_NAMES: tuple[str, str, str, str] = (
+    "mondio",
+    "scaled_detdio",
+    "scaled_mondio",
+    "srcur",
+)
+
+DETECTOR_NAMES: tuple[str, str, str] = (
+    "p3",
+    "de",
+    "perkin",
+)
+INTEGRATION_METHODS: tuple[str, ...] = (
+    "no_csr_cython",
+    "bbox_csr_cython",
+    "full_csr_cython",
+    "no_csr_ocl_gpu",
+    "bbox_csr_ocl_gpu",
+    "full_csr_ocl_gpu",
+    "no_histogram_cython",
+    "bbox_histogram_cython",
+    "full_histogram_ocl_gpu",
+)
