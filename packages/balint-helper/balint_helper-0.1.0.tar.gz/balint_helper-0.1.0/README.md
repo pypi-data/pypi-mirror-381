d40aa00b92f40ede70b8f4fe5fe1aee137fc743c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 03:13:45 +0000

    Add package balint-helper/0.1.0/balint_helper-0.1.0.tar.gz

---
+++ packages/balint-helper/balint_helper-0.1.0.tar.gz/balint_helper-0.1.0/README.md
@@ -0,0 +1,3 @@
+# helper
+
+Egyszerű segítő modul: névre köszön, eldönti egy számról, hogy páros vagy páratlan.
---
+++ packages/balint-helper/balint_helper-0.1.0.tar.gz/balint_helper-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='balint-helper',
+    version='0.1.0',
+    description='Egyszerű segítő modul kezdőknek',
+    author='A Te Neved',
+    packages=find_packages(),
+    install_requires=[],
+)
