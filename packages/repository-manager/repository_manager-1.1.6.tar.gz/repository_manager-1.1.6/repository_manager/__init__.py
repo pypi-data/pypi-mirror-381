9aad69678bc220a7cb9ea543c3aba04b73934427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 15:04:49 +0000

    Add package repository-manager/1.1.6/repository_manager-1.1.6.tar.gz

---
+++ packages/repository-manager/repository_manager-1.1.6.tar.gz/repository_manager-1.1.6/repository_manager/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+from repository_manager.repository_manager import Git, repository_manager, setup_logging
+from repository_manager.repository_manager_mcp import repository_manager_mcp
+
+"""
+report-manager
+
+Manage your git projects
+"""
+
+
+__all__ = ["Git", "repository_manager", "setup_logging", "repository_manager_mcp"]
