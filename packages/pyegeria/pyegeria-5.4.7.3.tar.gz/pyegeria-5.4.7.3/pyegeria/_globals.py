bc1be40db805f86f27112420382f1dab3e3beb22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 14:04:54 +0000

    Add package pyegeria/5.4.7.3/pyegeria-5.4.7.3.tar.gz

---
+++ packages/pyegeria/pyegeria-5.4.7.3.tar.gz/pyegeria-5.4.7.3/pyegeria/_globals.py
@@ -0,0 +1,49 @@
+"""
+SPDX-License-Identifier: Apache-2.0
+Copyright Contributors to the ODPi Egeria project.
+"""
+
+"""
+This common file is used to set some global values and enumerations used by the overall package. 
+
+"""
+
+
+is_debug = False
+disable_ssl_warnings = True
+enable_ssl_check = False
+max_paging_size = 500
+default_time_out = 30
+DEBUG_LEVEL = "quiet"
+comment_types = (
+    "ANSWER",
+    "OTHER",
+    "QUESTION",
+    "STANDARD_COMMENT",
+    "SUGGESTION",
+    "USAGE_EXPERIENCE",
+)
+star_ratings = (
+    "FIVE_STARS",
+    "FOUR_STARS",
+    "NO_RECOMMENDATION",
+    "ONE_STAR",
+    "THREE_STARS",
+    "TWO_STARS",
+)
+
+TEMPLATE_GUIDS: dict = {}
+INTEGRATION_GUIDS: dict = {}
+
+NO_ELEMENTS_FOUND = "No elements found"
+NO_ASSETS_FOUND = "No assets found"
+NO_SERVERS_FOUND = "No servers found"
+NO_CATALOGS_FOUND = "No catalogs found"
+NO_GLOSSARIES_FOUND = "No glossaries found"
+NO_TERMS_FOUND = "No terms found"
+NO_CATEGORIES_FOUND = "No categories found"
+NO_ELEMENT_FOUND = "No element found"
+NO_PROJECTS_FOUND = "No projects found"
+NO_COLLECTION_FOUND = "No collection found"
+NO_GUID_RETURNED = "No guid returned"
+NO_MEMBERS_FOUND = "No members found"
