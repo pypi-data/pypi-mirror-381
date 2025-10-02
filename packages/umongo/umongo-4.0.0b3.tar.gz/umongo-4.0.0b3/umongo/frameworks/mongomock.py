c03cd769bffe6ce6aebda939c4b54700b795c84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:04:50 +0000

    Add package umongo/4.0.0b3/umongo-4.0.0b3.tar.gz

---
+++ packages/umongo/umongo-4.0.0b3.tar.gz/umongo-4.0.0b3/umongo/frameworks/mongomock.py
@@ -0,0 +1,32 @@
+from mongomock.collection import Cursor
+from mongomock.database import Database
+
+from ..document import DocumentImplementation
+from ..instance import Instance
+from .pymongo import BaseWrappedCursor, PyMongoBuilder, PyMongoDocument
+
+# Mongomock aims at working like pymongo
+
+
+class WrappedCursor(BaseWrappedCursor, Cursor):
+    __slots__ = ()
+
+
+class MongoMockDocument(PyMongoDocument):
+    __slots__ = ()
+    cursor_cls = WrappedCursor
+    opts = DocumentImplementation.opts
+
+
+class MongoMockBuilder(PyMongoBuilder):
+    BASE_DOCUMENT_CLS = MongoMockDocument
+
+
+class MongoMockInstance(Instance):
+    """:class:`umongo.instance.Instance` implementation for mongomock"""
+
+    BUILDER_CLS = MongoMockBuilder
+
+    @staticmethod
+    def is_compatible_with(db):
+        return isinstance(db, Database)
