21c33b9ab5ef83bbd4f785ad4f333865037c7295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 15:04:51 +0000

    Add package ci-starter/0.9.1/ci_starter-0.9.1-py3-none-any.whl

---
+++ packages/ci-starter/ci_starter-0.9.1-py3-none-any.whl/ci_starter/__init__.py
@@ -0,0 +1,45 @@
+from collections.abc import Mapping
+from importlib.metadata import version as get_version
+from logging import getLogger
+from pathlib import Path
+
+from .asset_getter import get_asset
+from .constants import BASE_WORKFLOW_ASSET_PATH, HELPER_SCRIPT_ASSET_PATH
+from .git_helpers import get_repo_name
+from .presets import DISTRIBUTION_ARTIFACTS_DIR, LOCKFILE_ARTIFACT
+from .semantic_release_config import SemanticReleaseConfiguration
+from .utils import from_yaml
+
+__version__ = get_version(__package__)
+
+logger = getLogger(__name__)
+
+
+def generate_semantic_release_config(project_repo_path: Path) -> None:
+    repo_name = get_repo_name(project_repo_path)
+
+    config = SemanticReleaseConfiguration(
+        project_repo_path, LOCKFILE_ARTIFACT, repo_name, DISTRIBUTION_ARTIFACTS_DIR
+    )
+    config.write()
+
+
+def generate_helper_script() -> str:
+    script: str = get_asset(HELPER_SCRIPT_ASSET_PATH)
+    return script
+
+
+def generate_base_workflow(**kwargs: Mapping[str, str]) -> dict:
+    workflow: str = get_asset(BASE_WORKFLOW_ASSET_PATH)
+    yaml = from_yaml(workflow)
+
+    env_dict = {k.upper(): v for k, v in kwargs.items()}
+    yaml["env"].update(env_dict)
+
+    return yaml
+
+
+def generate_reusable_workflow(asset_path: Path) -> dict:
+    workflow: str = get_asset(asset_path)
+    yaml = from_yaml(workflow)
+    return yaml
