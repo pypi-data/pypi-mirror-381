c9bb966dade32477c1ce9d5620c71fabea4db20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 17:05:27 +0000

    Add package pyhackgui/1.0.0/pyhackgui-1.0.0-py3-none-any.whl

---
+++ packages/pyhackgui/pyhackgui-1.0.0-py3-none-any.whl/pyhackgui/__init__.py
@@ -0,0 +1,16 @@
+"""
+PyHackGUI - 一个专为游戏外挂界面设计的Python GUI库
+
+基于pygame构建，支持透明窗口、高DPI、中文显示等特性。
+通过PYHACKGUI库，您可以简单地绘制一个HACK界面。
+"""
+
+from .core import PyHackGUI
+
+__version__ = "1.0.0"
+__author__ = "LiangYu"
+__email__ = "liangyu0611@gmail.com"
+__description__ = "通过PYHACKGUI库，您可以简单地绘制一个HACK界面"
+
+# 导出主要类
+__all__ = ['PyHackGUI']
