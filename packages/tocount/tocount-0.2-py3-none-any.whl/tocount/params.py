b366d401c3f28143c24067162917eabcbab2f867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 15:04:33 +0000

    Add package tocount/0.2/tocount-0.2-py3-none-any.whl

---
+++ packages/tocount/tocount-0.2-py3-none-any.whl/tocount/params.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Tocount parameters and constants."""
+
+TOCOUNT_VERSION = "0.2"
+
+INVALID_TEXT_ESTIMATOR_MESSAGE = "Invalid value. `estimator` must be an instance of TextEstimator enum."
+INVALID_TEXT_MESSAGE = "Invalid value. `text` must be a string."
+
+LINEAR_MODELS = {
+    "english": {"a": 0.22027472695240083, "b": 1.3098454987590542},
+    "all": {"a": 0.24897308965467127, "b": 4.5430826510558830}
+}
