40da619e88cdca6a81fbd730287d90fbb95483b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 16:05:02 +0000

    Add package hostinger-api/0.0.19/hostinger_api-0.0.19.tar.gz

---
+++ packages/hostinger-api/hostinger_api-0.0.19.tar.gz/hostinger_api-0.0.19/hostinger_api/api/__init__.py
@@ -0,0 +1,28 @@
+# flake8: noqa
+
+# import apis into api package
+from hostinger_api.api.billing_catalog_api import BillingCatalogApi
+from hostinger_api.api.billing_orders_api import BillingOrdersApi
+from hostinger_api.api.billing_payment_methods_api import BillingPaymentMethodsApi
+from hostinger_api.api.billing_subscriptions_api import BillingSubscriptionsApi
+from hostinger_api.api.dns_snapshot_api import DNSSnapshotApi
+from hostinger_api.api.dns_zone_api import DNSZoneApi
+from hostinger_api.api.domains_availability_api import DomainsAvailabilityApi
+from hostinger_api.api.domains_forwarding_api import DomainsForwardingApi
+from hostinger_api.api.domains_portfolio_api import DomainsPortfolioApi
+from hostinger_api.api.domains_whois_api import DomainsWHOISApi
+from hostinger_api.api.reach_contacts_api import ReachContactsApi
+from hostinger_api.api.vps_actions_api import VPSActionsApi
+from hostinger_api.api.vps_backups_api import VPSBackupsApi
+from hostinger_api.api.vps_data_centers_api import VPSDataCentersApi
+from hostinger_api.api.vps_docker_manager_api import VPSDockerManagerApi
+from hostinger_api.api.vps_firewall_api import VPSFirewallApi
+from hostinger_api.api.vps_malware_scanner_api import VPSMalwareScannerApi
+from hostinger_api.api.vpsos_templates_api import VPSOSTemplatesApi
+from hostinger_api.api.vpsptr_records_api import VPSPTRRecordsApi
+from hostinger_api.api.vps_post_install_scripts_api import VPSPostInstallScriptsApi
+from hostinger_api.api.vps_public_keys_api import VPSPublicKeysApi
+from hostinger_api.api.vps_recovery_api import VPSRecoveryApi
+from hostinger_api.api.vps_snapshots_api import VPSSnapshotsApi
+from hostinger_api.api.vps_virtual_machine_api import VPSVirtualMachineApi
+
