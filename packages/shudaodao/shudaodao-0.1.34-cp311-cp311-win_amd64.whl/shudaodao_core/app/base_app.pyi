c68c8e20bcd9857c640dbf0b27037b1058044595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 15:04:16 +0000

    Add package shudaodao/0.1.34/shudaodao-0.1.34-cp311-cp311-win_amd64.whl

---
+++ packages/shudaodao/shudaodao-0.1.34-cp311-cp311-win_amd64.whl/shudaodao_core/app/base_app.pyi
@@ -0,0 +1,24 @@
+import abc
+from ..auth.auth_router import AuthRouter as AuthRouter
+from ..config.app_config import AppConfig as AppConfig
+from ..config.schemas.routers import RouterConfigSetting as RouterConfigSetting
+from ..license.verify import verify_license as verify_license
+from ..logger.logging_ import logging as logging
+from ..portal_auth import auth_registry as auth_registry
+from ..services.casbin_service import PermissionService as PermissionService
+from ..tools.class_scaner import ClassScanner as ClassScanner
+from ..tools.database_checker import DatabaseChecker as DatabaseChecker
+from ..utils.core_utils import CoreUtil as CoreUtil
+from _typeshed import Incomplete
+from abc import ABC, abstractmethod
+from fastapi import FastAPI
+
+class BaseApplication(ABC, metaclass=abc.ABCMeta):
+    app: Incomplete
+    def __init__(self) -> None: ...
+    @abstractmethod
+    def application_init(self, app: FastAPI) -> None: ...
+    @abstractmethod
+    async def application_load(self): ...
+    @abstractmethod
+    async def application_unload(self): ...
