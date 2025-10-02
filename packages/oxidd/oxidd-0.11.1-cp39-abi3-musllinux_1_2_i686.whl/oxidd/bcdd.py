bc8da821d847401725e5f9094d9806cf2e7f42f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 05:04:18 +0000

    Add package oxidd/0.11.1/oxidd-0.11.1-cp39-abi3-musllinux_1_2_i686.whl

---
+++ packages/oxidd/oxidd-0.11.1-cp39-abi3-musllinux_1_2_i686.whl/oxidd/bcdd.py
@@ -0,0 +1,5 @@
+"""Binary decision diagrams with complement edges (BCDDs)."""
+
+__all__ = ["BCDDFunction", "BCDDManager", "BCDDSubstitution"]
+
+from ._oxidd import BCDDFunction, BCDDManager, BCDDSubstitution
