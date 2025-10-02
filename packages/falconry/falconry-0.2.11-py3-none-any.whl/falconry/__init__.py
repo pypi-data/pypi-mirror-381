ffd1ce14b55a27268f9de521b5bb6a1fa44fa6f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 17:05:25 +0000

    Add package falconry/0.2.11/falconry-0.2.11-py3-none-any.whl

---
+++ packages/falconry/falconry-0.2.11-py3-none-any.whl/falconry/__init__.py
@@ -0,0 +1,6 @@
+from .manager import manager, Counter  # NOQA
+from .job import job  # NOQA
+from .status import FalconryStatus  # NOQA
+from . import cli  # NOQA
+from .schedd_wrapper import ScheddWrapper, kerberos_auth  # NOQA
+from .__main__ import config  # NOQA
