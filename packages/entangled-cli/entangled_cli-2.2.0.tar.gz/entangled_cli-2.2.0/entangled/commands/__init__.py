73f5757de19dd3a38e6e13d9599d707d58073823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 16:04:39 +0000

    Add package entangled-cli/2.2.0/entangled_cli-2.2.0.tar.gz

---
+++ packages/entangled-cli/entangled_cli-2.2.0.tar.gz/entangled_cli-2.2.0/entangled/commands/__init__.py
@@ -0,0 +1,17 @@
+from .new import new
+from .status import status
+from .stitch import stitch
+from .sync import sync
+from .tangle import tangle
+from .watch import watch
+from .brei import brei
+
+__all__ = [
+    "new",
+    "brei",
+    "status",
+    "stitch",
+    "sync",
+    "tangle",
+    "watch",
+]
---
+++ packages/entangled-cli/entangled_cli-2.2.0.tar.gz/entangled_cli-2.2.0/entangled/commands/sync.py
@@ -0,0 +1,50 @@
+from typing import Optional, Callable
+from itertools import chain
+from pathlib import Path
+
+import logging
+
+from ..filedb import file_db
+from ..config import config
+from .stitch import stitch, get_input_files
+from .tangle import tangle
+
+
+def _stitch_then_tangle():
+    stitch()
+    tangle()
+
+
+def sync_action() -> Optional[Callable[[], None]]:
+    input_file_list = get_input_files()
+
+    with file_db(readonly=True) as db:
+        changed = set(db.changed())
+
+        if not all(f in db for f in input_file_list):
+            return tangle
+
+        if not changed:
+            return None
+
+        if changed.isdisjoint(db.managed):
+            logging.info("Tangling")
+            return tangle
+
+        if changed.issubset(db.managed):
+            logging.info("Stitching")
+            return _stitch_then_tangle
+
+        logging.error("changed: %s", [str(p) for p in changed])
+        logging.error(
+            "Both markdown and code seem to have changed. " "Don't know what to do now."
+        )
+        return None
+
+
+def sync():
+    """Be smart wether to tangle or stich"""
+    config.read()
+    action = sync_action()
+    if action is not None:
+        action()
