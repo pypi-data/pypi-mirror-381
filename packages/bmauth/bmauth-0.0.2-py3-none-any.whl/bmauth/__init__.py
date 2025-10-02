0ebfb126ab3a86f61fe4b41951f8c48e6c624689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 15:04:33 +0000

    Add package bmauth/0.0.2/bmauth-0.0.2-py3-none-any.whl

---
+++ packages/bmauth/bmauth-0.0.2-py3-none-any.whl/bmauth/__init__.py
@@ -0,0 +1,11 @@
+"""
+BMAuth - Biometric Authentication System for FastAPI
+"""
+
+__version__ = "0.0.2"
+__author__ = "Sami Melhem"
+__email__ = "SaMiLMelhem23@gmail.com"
+
+# Import main components here as the library develops
+# from .auth import BMAuth
+# from .models import User, Device
