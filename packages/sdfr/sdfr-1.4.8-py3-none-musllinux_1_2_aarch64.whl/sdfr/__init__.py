737c7ae9302dc8e0b89660f0e15d67941aa60ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 04:04:36 +0000

    Add package sdfr/1.4.8/sdfr-1.4.8-py3-none-musllinux_1_2_aarch64.whl

---
+++ packages/sdfr/sdfr-1.4.8-py3-none-musllinux_1_2_aarch64.whl/sdfr/__init__.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright 2022 University of Warwick, University of York
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .SDF import new, read
+from . import sdf_helper
+from ._commit_info import (
+    __commit_date__,
+    __commit_id__,
+)
+from .loadlib import (
+    __library_commit_date__,
+    __library_commit_id__,
+)
+
+from importlib.metadata import version
+
+_module_name = "sdfr"
+
+try:
+    __version__ = version(_module_name)
+except Exception:
+    __version__ = "0.0.0"
+
+__all__ = [
+    "SDF",
+    "new",
+    "read",
+    "sdf_helper",
+    "__library_commit_date__",
+    "__library_commit_id__",
+    "__version__",
+    "__commit_date__",
+    "__commit_id__",
+]
