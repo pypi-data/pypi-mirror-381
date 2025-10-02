dbe6e073193a939ba76984900f936333a74ea1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-02 16:05:26 +0000

    Add package ctu-crs/1.0.5/ctu_crs-1.0.5.tar.gz

---
+++ packages/ctu-crs/ctu_crs-1.0.5.tar.gz/ctu_crs-1.0.5/src/ctu_crs/crs97.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+# Copyright (c) CTU -- All Rights Reserved
+# Created on: 2024-10-30
+#     Author: Vladimir Petrik <vladimir.petrik@cvut.cz>
+#
+from pathlib import Path
+import yaml
+from ctu_crs.crs_robot import CRSRobot
+
+
+class CRS97(CRSRobot):
+    def __init__(self, tty_dev: str | None = "/dev/mars", baudrate: int = 19200):
+        yaml_path = Path(__file__).parent / "params_crs97.yaml"
+        with open(yaml_path, "r") as f:
+            crs_params = yaml.safe_load(f)
+        super().__init__(tty_dev, baudrate, **crs_params)
