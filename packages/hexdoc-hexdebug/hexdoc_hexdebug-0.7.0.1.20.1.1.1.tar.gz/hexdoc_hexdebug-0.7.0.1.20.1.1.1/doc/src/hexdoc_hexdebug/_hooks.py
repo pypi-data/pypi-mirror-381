091ed1f7a8bdf34b657eda1dca184ef749a57626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 16:05:25 +0000

    Add package hexdoc-hexdebug/0.7.0.1.20.1.1.1/hexdoc_hexdebug-0.7.0.1.20.1.1.1.tar.gz

---
+++ packages/hexdoc-hexdebug/hexdoc_hexdebug-0.7.0.1.20.1.1.1.tar.gz/hexdoc_hexdebug-0.7.0.1.20.1.1.1/doc/src/hexdoc_hexdebug/_hooks.py
@@ -0,0 +1,64 @@
+from importlib.resources import Package
+
+from hexdoc.plugin import (
+    HookReturn,
+    LoadTaggedUnionsImpl,
+    ModPlugin,
+    ModPluginImpl,
+    ModPluginWithBook,
+    hookimpl,
+)
+from typing_extensions import override
+
+import hexdoc_hexdebug
+
+from . import recipes
+from .__gradle_version__ import FULL_VERSION, MINECRAFT_VERSION, MOD_VERSION
+from .__version__ import PY_VERSION
+
+
+class HexDebugPlugin(LoadTaggedUnionsImpl, ModPluginImpl):
+    @staticmethod
+    @hookimpl
+    def hexdoc_load_tagged_unions() -> HookReturn[Package]:
+        return [recipes]
+
+    @staticmethod
+    @hookimpl
+    def hexdoc_mod_plugin(branch: str) -> ModPlugin:
+        return HexDebugModPlugin(branch=branch)
+
+
+class HexDebugModPlugin(ModPluginWithBook):
+    @property
+    @override
+    def modid(self) -> str:
+        return "hexdebug"
+
+    @property
+    @override
+    def full_version(self) -> str:
+        return FULL_VERSION
+
+    @property
+    @override
+    def mod_version(self) -> str:
+        return f"{MOD_VERSION}+{MINECRAFT_VERSION}"
+
+    @property
+    @override
+    def plugin_version(self) -> str:
+        return PY_VERSION
+
+    @override
+    def resource_dirs(self) -> HookReturn[Package]:
+        # lazy import because generated may not exist when this file is loaded
+        # eg. when generating the contents of generated
+        # so we only want to import it if we actually need it
+        from ._export import generated
+
+        return generated
+
+    @override
+    def jinja_template_root(self) -> tuple[Package, str]:
+        return hexdoc_hexdebug, "_templates"
