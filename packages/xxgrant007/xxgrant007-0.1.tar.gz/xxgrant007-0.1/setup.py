e800e453a6a03aeec8ab63538c5bf1a64d2f3daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 15:06:42 +0000

    Add package xxgrant007/0.1/xxgrant007-0.1.tar.gz

---
+++ packages/xxgrant007/xxgrant007-0.1.tar.gz/xxgrant007-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xxgrant007', # Nombre de tu paquete
+    version='0.1', # Versión de tu paquete
+    packages=find_packages(where='src'),
+    description='geoestadistica',
+    author='fede',
+    author_email='fededimant@hotmail.com',
+    url='https://github.com/fededimant/xxgrant007', # URL del repositorio
+    # Encuentra automáticamente los paquetes
+  
+)
