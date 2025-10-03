ceb8e77ea1a435ec4ecf8980f5a3e41943b7b28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 16:05:02 +0000

    Add package assemblypy/0.0.6/assemblypy-0.0.6.tar.gz

---
+++ packages/assemblypy/assemblypy-0.0.6.tar.gz/assemblypy-0.0.6/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="assemblypy",
+    version="0.0.6",
+    author="Tamim Almutairi (tiktok:8ov.l)",
+    description="Program Assembly in Python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows :: Windows 11",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
