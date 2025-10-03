0c0a6e375543bff7c106cccd94e7aa2036ee5c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 05:05:19 +0000

    Add package ob-metaflow-stubs/6.0.10.13/ob_metaflow_stubs-6.0.10.13-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.10.13-py2.py3-none-any.whl/metaflow-stubs/mf_extensions/outerbounds/plugins/apps/core/code_package/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.10.1+obcheckpoint(0.2.8);ob(v1)                                                   #
+# Generated on 2025-10-02T16:55:23.730313                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import code_packager as code_packager
+from .code_packager import CodePackager as CodePackager
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.10.13-py2.py3-none-any.whl/metaflow-stubs/plugins/exit_hook/__init__.pyi
@@ -0,0 +1,11 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.10.1+obcheckpoint(0.2.8);ob(v1)                                                   #
+# Generated on 2025-10-02T16:55:23.713787                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import exit_hook_decorator as exit_hook_decorator
+
