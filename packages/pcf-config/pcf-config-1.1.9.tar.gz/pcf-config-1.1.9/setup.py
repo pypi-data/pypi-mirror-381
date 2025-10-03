a2e1a222611db8ede75a1867b2abb1147c810333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 04:05:36 +0000

    Add package pcf-config/1.1.9/pcf-config-1.1.9.tar.gz

---
+++ packages/pcf-config/pcf-config-1.1.9.tar.gz/pcf-config-1.1.9/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Setup script for pcf-config package.
+This file is kept for backward compatibility, but pyproject.toml is the primary configuration.
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
