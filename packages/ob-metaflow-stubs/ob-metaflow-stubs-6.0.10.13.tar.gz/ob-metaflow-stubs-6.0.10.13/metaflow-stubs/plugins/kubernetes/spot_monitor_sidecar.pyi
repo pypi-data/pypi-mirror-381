b894fe0137d2014896e7565d02a4d7c81c38ba03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 05:05:20 +0000

    Add package ob-metaflow-stubs/6.0.10.13/ob-metaflow-stubs-6.0.10.13.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.10.13.tar.gz/ob-metaflow-stubs-6.0.10.13/metaflow-stubs/plugins/kubernetes/spot_monitor_sidecar.pyi
@@ -0,0 +1,20 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.10.1+obcheckpoint(0.2.8);ob(v1)                                                   #
+# Generated on 2025-10-02T16:55:23.747936                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+class SpotTerminationMonitorSidecar(object, metaclass=type):
+    def __init__(self):
+        ...
+    def process_message(self, msg):
+        ...
+    @classmethod
+    def get_worker(cls):
+        ...
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.10.13.tar.gz/ob-metaflow-stubs-6.0.10.13/metaflow-stubs/plugins/parsers.pyi
@@ -0,0 +1,25 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.10.1+obcheckpoint(0.2.8);ob(v1)                                                   #
+# Generated on 2025-10-02T16:55:23.673298                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from .._vendor import yaml as yaml
+
+def yaml_parser(content: str) -> dict:
+    """
+    Parse YAML content to a dictionary.
+    
+    Parameters
+    ----------
+    content : str
+    
+    Returns
+    -------
+    dict
+    """
+    ...
+
