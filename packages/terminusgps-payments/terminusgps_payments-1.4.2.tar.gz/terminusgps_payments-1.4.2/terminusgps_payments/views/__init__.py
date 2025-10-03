183189456f782ade2024fa5c61dfd40e70e0307e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 05:05:06 +0000

    Add package terminusgps-payments/1.4.2/terminusgps_payments-1.4.2.tar.gz

---
+++ packages/terminusgps-payments/terminusgps_payments-1.4.2.tar.gz/terminusgps_payments-1.4.2/terminusgps_payments/views/__init__.py
@@ -0,0 +1,18 @@
+from .address_profiles import (
+    AddressProfileCreateView,
+    AddressProfileDeleteView,
+    AddressProfileDetailView,
+    AddressProfileListView,
+)
+from .payment_profiles import (
+    PaymentProfileCreateView,
+    PaymentProfileDeleteView,
+    PaymentProfileDetailView,
+    PaymentProfileListView,
+)
+from .subscriptions import (
+    SubscriptionDeleteView,
+    SubscriptionDetailView,
+    SubscriptionListView,
+    SubscriptionUpdateView,
+)
