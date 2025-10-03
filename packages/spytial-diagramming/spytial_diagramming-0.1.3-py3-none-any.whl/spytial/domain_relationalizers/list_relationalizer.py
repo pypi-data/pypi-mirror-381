934e493a193884447cd2660c352d244c02ac5c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 06:05:19 +0000

    Add package spytial-diagramming/0.1.3/spytial_diagramming-0.1.3-py3-none-any.whl

---
+++ packages/spytial-diagramming/spytial_diagramming-0.1.3-py3-none-any.whl/spytial/domain_relationalizers/list_relationalizer.py
@@ -0,0 +1,38 @@
+"""Relationalizer for listobjects."""
+
+from typing import Any, List, Tuple
+from .base import RelationalizerBase, Atom, Relation
+
+
+class ListRelationalizer(RelationalizerBase):
+    """Handles list objects."""
+
+    def can_handle(self, obj: Any) -> bool:
+        return isinstance(obj, list)
+
+    def relationalize(self, obj: Any, walker_func) -> Tuple[List[Atom], List[Relation]]:
+        obj_id = walker_func._get_id(obj)
+        typ = type(obj).__name__
+        atom = Atom(id=obj_id, type=typ, label=f"{typ}[{len(obj)}]")
+
+        atoms = [atom]
+        relations = []
+        for i, elt in enumerate(obj):
+            # Create an atom for the index
+            idx_id = walker_func._get_id(i)
+
+            # Wait, only create the atom for the index if it's not already created?
+
+
+            idx_atom = Atom(id=idx_id, type="int", label=str(i))
+            atoms.append(idx_atom)
+            
+            # Get the element ID
+            eid = walker_func(elt)
+            
+            # Create a ternary relation: idx(list, index, element)
+            relations.append(Relation("idx", [obj_id, idx_id, eid]))
+
+        return atoms, relations
+
+
