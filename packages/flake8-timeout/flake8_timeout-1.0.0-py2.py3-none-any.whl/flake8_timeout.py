f5017d2bd05cf8d6716279d7b941965298e3e022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 03:14:30 +0000

    Add package flake8-timeout/1.0.0/flake8_timeout-1.0.0-py2.py3-none-any.whl

---
+++ packages/flake8-timeout/flake8_timeout-1.0.0-py2.py3-none-any.whl/flake8_timeout.py
@@ -0,0 +1,71 @@
+import ast
+import importlib.metadata as importlib_metadata
+from collections.abc import Generator
+from typing import Any
+
+MSG = 'TIM100 request call has no timeout'
+
+METHODS = [
+    'request', 'get', 'head', 'post',
+    'patch', 'put', 'delete', 'options',
+]
+
+
+class Visitor(ast.NodeVisitor):
+    def __init__(self) -> None:
+        self.assignments: list[tuple[int, int]] = []
+
+    def visit_Call(self, node: ast.Call) -> None:
+        if (
+                isinstance(node, ast.Call) and
+                isinstance(node.func, ast.Attribute) and
+                isinstance(node.func.value, ast.Name) and
+                node.func.value.id == 'requests' and
+                node.func.attr in METHODS
+        ):
+            for kwarg in node.keywords:
+                if (
+                        kwarg.arg == 'timeout' and
+                        isinstance(kwarg.value, ast.Constant) and
+                        kwarg.value.value is not None
+                ):
+                    break
+            else:
+                self.assignments.append((node.lineno, node.col_offset))
+        elif (
+            isinstance(node, ast.Call) and
+            isinstance(node.func, ast.Attribute) and
+            isinstance(node.func.value, ast.Attribute) and
+            isinstance(node.func.value.value, ast.Name) and
+            node.func.value.value.id == 'urllib' and
+            node.func.value.attr == 'request' and
+            node.func.attr == 'urlopen'
+        ):
+            for kwarg in node.keywords:
+                if (
+                    kwarg.arg == 'timeout' and
+                    isinstance(kwarg.value, ast.Constant) and
+                    kwarg.value.value is not None
+                ):
+                    break
+            else:
+                # check if it was passed as a positional argument instead
+                # args are: (url, data=None, [timeout, ]*, cafile=None ...
+                if len(node.args) < 3:
+                    self.assignments.append((node.lineno, node.col_offset))
+
+        self.generic_visit(node)
+
+
+class Plugin:
+    name = __name__
+    version = importlib_metadata.version(__name__)
+
+    def __init__(self, tree: ast.AST):
+        self._tree = tree
+
+    def run(self) -> Generator[tuple[int, int, str, type[Any]], None, None]:
+        visitor = Visitor()
+        visitor.visit(self._tree)
+        for line, col in visitor.assignments:
+            yield line, col, MSG, type(self)
