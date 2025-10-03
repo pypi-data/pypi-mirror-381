0c8466f0a767795a0d307c1b6a43f66b66feba60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 07:05:30 +0000

    Add package nevu-ui/0.6.0/nevu_ui-0.6.0-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/nevu-ui/nevu_ui-0.6.0-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/nevu_ui/widgets/__init__.py
@@ -0,0 +1,13 @@
+from .widget import Widget
+from .label import Label
+from .button import Button
+from .empty_widget import EmptyWidget
+from .rect_checkbox import RectCheckBox
+from .image import Image
+from .gif import Gif
+from .input import Input
+from .music_player import MusicPlayer
+from .element_switcher import ElementSwitcher, Element
+__all__ = [
+    'Widget', 'Label', 'Button', 'EmptyWidget', 'RectCheckBox', 'Image', 'Gif', 'Input', 'MusicPlayer', 'ElementSwitcher', 'Element'
+]
