172d913dbbfc0bb03670055497237238fd5b4b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 03:14:42 +0000

    Add package screenshotbase/0.1.1/screenshotbase-0.1.1.tar.gz

---
+++ packages/screenshotbase/screenshotbase-0.1.1.tar.gz/screenshotbase-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="screenshotbase",
+    version="0.1.1",
+    description="Python client for screenshotbase.com API",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="ScreenshotBase",
+    author_email="support@screenshotbase.com",
+    url="https://screenshotbase.com/docs/",
+    packages=find_packages(exclude=("tests", "examples")),
+    python_requires=">=3.8",
+    install_requires=[
+        "requests>=2.31.0",
+    ],
+    license="MIT",
+    license_files=("LICENSE",),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    project_urls={
+        "Documentation": "https://screenshotbase.com/docs/",
+    },
+    include_package_data=True,
+)
+
+
