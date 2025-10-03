75415c946994f29ebbe9c4cfb532f36f2e0d3bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 16:05:32 +0000

    Add package gllm-agents-binary/0.4.21/gllm_agents_binary-0.4.21-cp312-cp312-macosx_13_0_x86_64.whl

---
+++ packages/gllm-agents-binary/gllm_agents_binary-0.4.21-cp312-cp312-macosx_13_0_x86_64.whl/gllm_agents/examples/hello_world_a2a_langgraph_artifact_client.pyi
@@ -0,0 +1,5 @@
+from gllm_agents.agent import LangGraphAgent as LangGraphAgent
+from gllm_agents.agent.types import A2AClientConfig as A2AClientConfig
+
+async def main() -> None:
+    """Main function demonstrating the General Assistant agent with A2A capabilities."""
