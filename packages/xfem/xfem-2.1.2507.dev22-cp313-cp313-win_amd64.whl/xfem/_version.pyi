1a5545a59991a29b1904f51fb93a878833b51bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 16:05:20 +0000

    Add package xfem/2.1.2507.dev22/xfem-2.1.2507.dev22-cp313-cp313-win_amd64.whl

---
+++ packages/xfem/xfem-2.1.2507.dev22-cp313-cp313-win_amd64.whl/xfem/_version.pyi
@@ -0,0 +1,10 @@
+from __future__ import annotations
+from builtins import object as VERSION_TUPLE
+from builtins import object as COMMIT_ID
+__all__: list = ['__version__', '__version_tuple__', 'version', 'version_tuple', '__commit_id__', 'commit_id']
+__commit_id__: str = 'g804b0777f'
+__version__: str = '2.1.2507.dev22'
+__version_tuple__: tuple = (2, 1, 2507, 'dev22')
+commit_id: str = 'g804b0777f'
+version: str = '2.1.2507.dev22'
+version_tuple: tuple = (2, 1, 2507, 'dev22')
