4a5f65062b7086a53668d6995d089cb6c3940104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 07:05:25 +0000

    Add package windmill-api/1.554.1/windmill_api-1.554.1-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.554.1-py3-none-any.whl/windmill_api/api/setting/databases_exist.py
@@ -0,0 +1,149 @@
+from http import HTTPStatus
+from typing import Any, Dict, List, Optional, Union, cast
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...types import Response
+
+
+def _get_kwargs(
+    *,
+    json_body: List[str],
+) -> Dict[str, Any]:
+    pass
+
+    json_json_body = json_body
+
+    return {
+        "method": "post",
+        "url": "/settings/databases_exist",
+        "json": json_json_body,
+    }
+
+
+def _parse_response(*, client: Union[AuthenticatedClient, Client], response: httpx.Response) -> Optional[List[str]]:
+    if response.status_code == HTTPStatus.OK:
+        response_200 = cast(List[str], response.json())
+
+        return response_200
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(*, client: Union[AuthenticatedClient, Client], response: httpx.Response) -> Response[List[str]]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    json_body: List[str],
+) -> Response[List[str]]:
+    """checks that all given databases exist or else return the ones that don't
+
+    Args:
+        json_body (List[str]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[List[str]]
+    """
+
+    kwargs = _get_kwargs(
+        json_body=json_body,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    json_body: List[str],
+) -> Optional[List[str]]:
+    """checks that all given databases exist or else return the ones that don't
+
+    Args:
+        json_body (List[str]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        List[str]
+    """
+
+    return sync_detailed(
+        client=client,
+        json_body=json_body,
+    ).parsed
+
+
+async def asyncio_detailed(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    json_body: List[str],
+) -> Response[List[str]]:
+    """checks that all given databases exist or else return the ones that don't
+
+    Args:
+        json_body (List[str]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[List[str]]
+    """
+
+    kwargs = _get_kwargs(
+        json_body=json_body,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    json_body: List[str],
+) -> Optional[List[str]]:
+    """checks that all given databases exist or else return the ones that don't
+
+    Args:
+        json_body (List[str]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        List[str]
+    """
+
+    return (
+        await asyncio_detailed(
+            client=client,
+            json_body=json_body,
+        )
+    ).parsed
