f445e2882cfa5e653ab6d33bba4fc5523e28be18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 03:15:10 +0000

    Add package ya-business-api/2.0.0/ya_business_api-2.0.0.tar.gz

---
+++ packages/ya-business-api/ya_business_api-2.0.0.tar.gz/ya_business_api-2.0.0/ya_business_api/core/exceptions.py
@@ -0,0 +1,26 @@
+class APIError(Exception):
+	"""
+	Basic API error.
+	"""
+	pass
+
+
+class AuthenticationError(APIError):
+	"""
+	User authentication error.
+	"""
+	pass
+
+
+class CSRFTokenError(APIError):
+	"""
+	Invalid CSRF token error.
+	"""
+	pass
+
+
+class ParserError(APIError):
+	"""
+	Basic parser error.
+	"""
+	pass
