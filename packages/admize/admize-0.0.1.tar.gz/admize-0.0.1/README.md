99e3de3484239cf091f7699fecc25a8851e57676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 16:04:56 +0000

    Add package admize/0.0.1/admize-0.0.1.tar.gz

---
+++ packages/admize/admize-0.0.1.tar.gz/admize-0.0.1/README.md
@@ -0,0 +1,14 @@
+# Admize
+
+🚧 Under Development 🚧
+
+Admize is a custom Django admin enhancement project.  
+Currently in early development — not licensed for public use.
+
+## Status
+
+- Reserved project name on GitHub & PyPI
+- Development in progress
+- License: All rights reserved (temporary)
+
+For inquiries, contact: admize.dev@gmail.com
---
+++ packages/admize/admize-0.0.1.tar.gz/admize-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="admize",
+    version="0.0.1",
+    description="Custom Django admin enhancement library",
+    author="Vladyslav Tyzhbiriak",
+    author_email="admize.dev@gmail.com",
+    url="https://github.com/Vlad-tg/admize",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.8",
+)
