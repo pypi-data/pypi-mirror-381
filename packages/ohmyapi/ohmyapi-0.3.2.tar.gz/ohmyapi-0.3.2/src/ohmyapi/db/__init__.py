f3808a9b9718a457476c0cb9b01c28236c8512af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 03:15:09 +0000

    Add package ohmyapi/0.3.2/ohmyapi-0.3.2.tar.gz

---
+++ packages/ohmyapi/ohmyapi-0.3.2.tar.gz/ohmyapi-0.3.2/src/ohmyapi/db/__init__.py
@@ -0,0 +1,10 @@
+from tortoise.manager import Manager
+from tortoise.queryset import QuerySet
+from tortoise.signals import (
+    post_delete,
+    post_save,
+    pre_delete,
+    pre_save,
+)
+
+from .model import Model, field
