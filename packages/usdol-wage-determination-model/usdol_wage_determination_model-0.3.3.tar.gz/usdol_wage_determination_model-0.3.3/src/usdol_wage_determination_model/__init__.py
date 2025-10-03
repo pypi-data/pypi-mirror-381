45841fb1fc4cd3cb35f199028595db521aff904b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 04:05:20 +0000

    Add package usdol-wage-determination-model/0.3.3/usdol_wage_determination_model-0.3.3.tar.gz

---
+++ packages/usdol-wage-determination-model/usdol_wage_determination_model-0.3.3.tar.gz/usdol_wage_determination_model-0.3.3/src/usdol_wage_determination_model/__init__.py
@@ -0,0 +1,8 @@
+from .date_range import DateRange as DateRange  # noqa: F401
+from .job import Job as Job
+from .location import Location as Location
+from .wage import Wage as Wage
+from .zone import Zone as Zone
+
+from .wage_determination import ConstructionType as ConstructionType
+from .wage_determination import WageDetermination as WageDetermination
