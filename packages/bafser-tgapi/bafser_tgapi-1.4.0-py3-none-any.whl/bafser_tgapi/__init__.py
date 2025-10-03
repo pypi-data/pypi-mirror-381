4ca59571bc273110d50eec79b9d7bfcc2af0dc93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 06:04:43 +0000

    Add package bafser-tgapi/1.4.0/bafser_tgapi-1.4.0-py3-none-any.whl

---
+++ packages/bafser-tgapi/bafser_tgapi-1.4.0-py3-none-any.whl/bafser_tgapi/__init__.py
@@ -0,0 +1,8 @@
+from .bot import Bot, BotCmdArgs
+from .bot_with_db import BotWithDB
+from .db.msg import MsgBase
+from .db.user import TgUserBase
+from .methods import *
+from .types import *
+from .utils import (call_async, check_webhook_token, configure_webhook, get_bot_name, get_url, process_update, run_long_polling, set_webhook, setup,
+                    url_for)
