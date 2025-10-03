f850a0b10362d50fb48c5165216b1566e3db3740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 05:04:45 +0000

    Add package proteomicruler/0.1.5/proteomicruler-0.1.5-py3-none-any.whl

---
+++ packages/proteomicruler/proteomicruler-0.1.5-py3-none-any.whl/proteomicRuler/test_ruler.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+import pandas as pd
+
+from proteomicRuler.ruler import Ruler, add_mw
+
+
+class TestRuler(TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        self.da = pd.read_csv(
+            r"C:\Users\Toan Phung\Downloads\raw (20).txt",
+            sep="\t")
+
+    def test_init(self):
+        ruler = Ruler(self.da)
+        print(ruler.factor)
+        print(ruler.organism)
+        # ruler.load_genes()
+        # ruler.df.to_csv("test.csv")
+        # ruler.plot()
+
+
+class Test(TestCase):
+    def test_add_mw(self):
+        df = pd.read_csv(r"\\estorage.dundee.ac.uk\lfs-mrc-data\proteinchem\Fred Lamoliatte\Students\20250922_KadyPatton\20250924_KPatton_MSF_120556\report.pg_matrix.tsv", sep="\t")
+        # used as unique index and to directly fetch mw data from UniProt
+        mw_col = "Mass"
+        # molecular weight column name
+        ploidy = 2
+        # ploidy number
+        total_cellular_protein_concentration = 200
+        intensity_columns = [i for i in df.columns if i.startswith("\\")]
+        accession_id_col = "Protein.Group"
+        df = add_mw(df, accession_id_col)
+        df = df[pd.notnull(df[mw_col])]
+        df[mw_col] = df[mw_col].astype(float)
+        ruler = Ruler(df, intensity_columns, mw_col, accession_id_col, ploidy, total_cellular_protein_concentration) #
+        ruler.df.to_csv("output.txt", sep="\t", index=False)
