20f11b03e75210d01e4be71f2304b228593abe05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 05:04:53 +0000

    Add package stratdev/1.0.1/stratdev-1.0.1-py3-none-any.whl

---
+++ packages/stratdev/stratdev-1.0.1-py3-none-any.whl/stratdev/backtesting/bt_file_content.py
@@ -0,0 +1,60 @@
+
+"""
+Content for backtesting session py files
+"""
+
+strategy = (
+"""
+import pandas as pd
+from datetime import datetime, time
+from backtesting import Strategy, Backtest
+
+
+class StratName(Strategy):
+    """
+    
+    """
+
+    def init(self):
+        pass
+
+
+    def next(self):
+
+        price = self.data.Close[-1]
+
+
+        if not self.position:
+            pass
+"""
+)
+
+data_prep = (
+"""
+from stratdev.utilities.get_utils import get_dfs
+
+ohlc_csv_path = '../ohlc_csv/'
+updated_ohlc_path = './ohlc/'
+period = ''
+interval = ''
+
+dfs = get_dfs(ohlc_csv_path, period, interval)   
+
+for ticker in dfs.keys():
+    df = dfs[ticker]
+    # Modify or add to DataFrame (calculations, indicators, etc)
+    df.to_csv(f'{ohlc_csv_path}{ticker}_{period}_{interval}_updated.csv')
+
+updated_dfs = get_dfs(updated_ohlc_path, period, interval)
+"""
+)
+
+backtest = (
+"""
+from stratdev.backtesting.run_backtest import backtest_analysis   
+from strategy import StratName 
+from data_prep import updated_dfs 
+
+backtest_analysis(StratName, updated_dfs)
+"""
+)
