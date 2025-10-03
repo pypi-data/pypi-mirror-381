47bc63aeed0d5e52c72c6fe4ebb4e7b878fa4ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 06:05:34 +0000

    Add package fabricconnector/0.0.1/fabricconnector-0.0.1-py3-none-any.whl

---
+++ packages/fabricconnector/fabricconnector-0.0.1-py3-none-any.whl/fabricconnector/__init__.py
@@ -0,0 +1,12 @@
+"""
+FabricConnector - Python connector library for Microsoft Fabric notebooks.
+
+This is a placeholder release to claim the package name on PyPI.
+The full production release is coming soon.
+
+For more information, visit: https://www.powerbiconnector.nl
+"""
+
+__version__ = "0.0.1"
+__author__ = "Davista"
+__email__ = "support@powerbiconnector.nl"
