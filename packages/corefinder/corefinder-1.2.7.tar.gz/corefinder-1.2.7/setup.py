0e5755fc37b696885c132db5ca8f072ca306fd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 03:14:31 +0000

    Add package corefinder/1.2.7/corefinder-1.2.7.tar.gz

---
+++ packages/corefinder/corefinder-1.2.7.tar.gz/corefinder-1.2.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="corefinder",
+    version="1.2.7",
+    author="Soumyajit Basu",
+    author_email="soumyajit.basu62@gmail.com",
+    description="A module for my digital business card",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Corefinder89/corefinder",
+    packages=['app'],
+    include_package_data=True,
+    entry_points={"console_scripts": ["corefinder=app.main:main"]},
+    zip_safe=False,
+    python_requires=">=3.6",
+    license="License :: OSI Approved :: MIT License",
+    classifiers=["Programming Language :: Python"]
+)
