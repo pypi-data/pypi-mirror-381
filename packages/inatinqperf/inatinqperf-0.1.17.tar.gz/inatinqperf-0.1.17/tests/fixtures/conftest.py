22b8cebe5877ff7816462eab6bc665b65eff6a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 06:05:49 +0000

    Add package inatinqperf/0.1.17/inatinqperf-0.1.17.tar.gz

---
+++ packages/inatinqperf/inatinqperf-0.1.17.tar.gz/inatinqperf-0.1.17/tests/fixtures/conftest.py
@@ -0,0 +1,11 @@
+"""conftest file for test data fixtures."""
+
+from pathlib import Path
+import yaml
+import pytest
+
+
+@pytest.fixture(name="benchmark_yaml")
+def benchmark_config_fixture(config_yaml: Path):
+    with config_yaml.open() as f:
+        return yaml.safe_load(f)
