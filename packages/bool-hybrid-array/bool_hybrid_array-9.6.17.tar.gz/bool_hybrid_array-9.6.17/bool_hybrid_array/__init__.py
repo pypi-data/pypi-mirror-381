7e9da95dfc2e685ff2597b29481d2d6886d89116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 16:05:18 +0000

    Add package bool-hybrid-array/9.6.17/bool_hybrid_array-9.6.17.tar.gz

---
+++ packages/bool-hybrid-array/bool_hybrid_array-9.6.17.tar.gz/bool_hybrid_array-9.6.17/bool_hybrid_array/__init__.py
@@ -0,0 +1,27 @@
+import sys
+from types import ModuleType
+from . import core
+from .core import builtins,__builtins__
+__version__ = "9.6.17"
+try:
+    original_builtins_dict = builtins.copy()
+    __builtins__ = ProtectedBuiltinsDict(original_builtins_dict)
+    builtins = __builtins__
+except Exception:
+    pass
+try:
+    core.__dict__ = ProtectedBuiltinsDict(core.__dict__)
+except Exception as e:
+    pass
+public_objects = []
+for name in dir(core):
+    if not name.startswith("_"):
+        obj = getattr(core, name)
+        if isinstance(obj, (type, ModuleType)) or callable(obj):
+            public_objects.append(name)
+__all__ = public_objects + ["__version__","__builtins__","core","builtins"]
+globals().update({
+    name: getattr(core, name)
+    for name in public_objects
+})
+
