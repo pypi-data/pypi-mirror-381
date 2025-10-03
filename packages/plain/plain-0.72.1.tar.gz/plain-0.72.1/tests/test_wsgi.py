5b780e1deae99daf6ad5966483261fcad52e6070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 07:05:12 +0000

    Add package plain/0.72.1/plain-0.72.1.tar.gz

---
+++ packages/plain/plain-0.72.1.tar.gz/plain-0.72.1/tests/test_wsgi.py
@@ -0,0 +1,22 @@
+from io import BytesIO
+
+from plain.internal.handlers.wsgi import WSGIHandler
+
+
+def test_wsgi_handler():
+    """
+    Test the default plain.wsgi.app import and
+    basic behavior with minimal environ input.
+    """
+    wsgi = WSGIHandler()
+    response = wsgi(
+        {
+            "REQUEST_METHOD": "GET",
+            "wsgi.input": BytesIO(b""),
+            "wsgi.url_scheme": "https",
+        },
+        lambda *args: None,
+    )
+
+    assert response.status_code == 200
+    assert response.content == b"Hello, world!"
