6ce3e746a27853ff30c7a5e6e4ea19b199555b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 05:05:23 +0000

    Add package dibbayajyotiroy-payroll-sdk/0.1.0/dibbayajyotiroy_payroll_sdk-0.1.0.tar.gz

---
+++ packages/dibbayajyotiroy-payroll-sdk/dibbayajyotiroy_payroll_sdk-0.1.0.tar.gz/dibbayajyotiroy_payroll_sdk-0.1.0/openapi_client/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.employees_api import EmployeesApi
+from openapi_client.api.payroll_automation_api import PayrollAutomationApi
+from openapi_client.api.roles_configuration_api import RolesConfigurationApi
+from openapi_client.api.placeholders_future_features_api import PlaceholdersFutureFeaturesApi
+
