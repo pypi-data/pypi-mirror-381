9c948b87d1a5efb18ca348be78feadcb34aac993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 16:04:59 +0000

    Add package yycore009/0.1/yycore009-0.1.tar.gz

---
+++ packages/yycore009/yycore009-0.1.tar.gz/yycore009-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='yycore009', # Nombre de tu paquete
+    packages=find_packages(),
+    version='0.1', # Versión de tu paquete
+    description='geoestadistica',
+    author='fede',
+    author_email='fededimant@hotmail.com',
+    url='https://github.com/fededimant/yycore009', # URL del repositorio
+    # Encuentra automáticamente los paquetes
+  
+)
