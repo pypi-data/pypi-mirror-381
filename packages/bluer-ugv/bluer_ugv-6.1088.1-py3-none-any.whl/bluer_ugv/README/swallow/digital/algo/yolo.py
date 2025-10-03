063f05e9cf0702d1b1f2a1a0aac5e1c59ab94113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 06:04:47 +0000

    Add package bluer-ugv/6.1088.1/bluer_ugv-6.1088.1-py3-none-any.whl

---
+++ packages/bluer-ugv/bluer_ugv-6.1088.1-py3-none-any.whl/bluer_ugv/README/swallow/digital/algo/yolo.py
@@ -0,0 +1,26 @@
+from bluer_objects.README.items import ImageItems
+from bluer_ugv.README.consts import assets, assets2_bluer_swallow
+
+items = [
+    {
+        "path": "../docs/bluer_swallow/digital/algo/yolo",
+        "items": ImageItems(
+            {
+                f"{assets2_bluer_swallow}/yolo-debug.png": "",
+            }
+        ),
+    },
+    {
+        "path": "../docs/bluer_swallow/digital/algo/yolo/train.md",
+        "items": ImageItems(
+            {
+                f"{assets2_bluer_swallow}/yolo-debug-{0}.png": "",
+                f"{assets2_bluer_swallow}/yolo-debug-{2}.png": "",
+                f"{assets2_bluer_swallow}/yolo-debug-{3}.png": "",
+                f"{assets2_bluer_swallow}/yolo-debug-{4}.png": "",
+                f"{assets2_bluer_swallow}/yolo-debug-{5}.png": "",
+                f"{assets}/swallow-debug-2025-09-16-19-53-19-4yzsp8/swallow-debug-2025-09-16-19-53-19-4yzsp8-2.gif": "",
+            }
+        ),
+    },
+]
