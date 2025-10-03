a220ad171cdc494d1abf95e4f31502ed3dfc4f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 15:05:10 +0000

    Add package peti007link/0.1/peti007link-0.1.tar.gz

---
+++ packages/peti007link/peti007link-0.1.tar.gz/peti007link-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='peti007link', # Nombre de tu paquete
+    version='0.1', # Versión de tu paquete
+    packages=find_packages(where='src'),
+    description='geoestadistica',
+    author='fede',
+    author_email='fededimant@hotmail.com',
+    url='https://github.com/fededimant/test_libreria', # URL del repositorio
+    # Encuentra automáticamente los paquetes
+  
+)
