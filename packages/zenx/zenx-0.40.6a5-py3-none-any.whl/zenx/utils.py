07f7990ff7d55713dbaeb9e3577305c0b7f9988f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 16:05:15 +0000

    Add package zenx/0.40.6a5/zenx-0.40.6a5-py3-none-any.whl

---
+++ packages/zenx/zenx-0.40.6a5-py3-none-any.whl/zenx/utils.py
@@ -0,0 +1,40 @@
+import asyncio
+import time
+import uuid
+from typing import Dict
+import functools
+
+
+def get_time() -> int:
+    """ current unix time in milliseconds """
+    return int(time.time() * 1000)
+
+
+def log_processing_time(func):
+    @functools.wraps(func)
+    async def wrapper(self, item: Dict, spider: str, *args, **kwargs) -> Dict:
+        start_time = get_time()
+        result = await func(self, item, spider)
+        end_time = get_time()
+        processed_time = end_time - start_time
+        self.logger.info("processed", id=item['_id'], time_ms=processed_time, pipeline=self.name, started_at=start_time, finished_at=end_time)
+        return result
+    return wrapper
+
+
+def get_uuid() -> str:
+    return uuid.uuid4().hex
+
+
+async def cancel_task(t: asyncio.Task) -> None:
+    if t and not t.done():
+        t.cancel()
+        try:
+            await t
+        except asyncio.CancelledError:
+            pass
+
+
+def get_real_time(monotonic_time: float) -> float:
+    real_time = monotonic_time + (time.time() - asyncio.get_running_loop().time())
+    return round(real_time, 3)
