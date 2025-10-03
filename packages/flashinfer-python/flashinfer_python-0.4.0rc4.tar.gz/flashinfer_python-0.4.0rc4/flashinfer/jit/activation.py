a3881e491415070542cf1ae229f99c8cb1daf29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 06:04:48 +0000

    Add package flashinfer-python/0.4.0rc4/flashinfer_python-0.4.0rc4.tar.gz

---
+++ packages/flashinfer-python/flashinfer_python-0.4.0rc4.tar.gz/flashinfer_python-0.4.0rc4/flashinfer/jit/activation.py
@@ -0,0 +1,88 @@
+"""
+Copyright (c) 2024 by FlashInfer team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+
+import jinja2
+
+from . import env as jit_env
+from .core import JitSpec, gen_jit_spec
+from .utils import write_if_different
+
+activation_templ = r"""
+#include <flashinfer/activation.cuh>
+#include <cuda_runtime.h>
+#include "tvm_ffi_utils.h"
+
+{% set func_name = act_func_name ~ '_and_mul' %}
+
+using namespace flashinfer;
+
+{{ act_func_def }}
+
+void {{ func_name }}(Tensor out, Tensor input, bool enable_pdl) {
+  int d = input->shape[input->ndim -1] / 2;
+  int64_t num_tokens = get_numel(input) / input->shape[input->ndim -1];
+  dim3 grid(num_tokens);
+
+  cudaSetDevice(out->device.device_id);
+  const cudaStream_t stream = get_stream(out->device);
+  DISPATCH_DLPACK_DTYPE_TO_CTYPE_FP16(input->dtype, c_type, [&] {
+    uint32_t vec_size = 16 / sizeof(c_type);
+    cudaLaunchConfig_t config;
+    config.gridDim = num_tokens;
+    config.blockDim = std::min(d / vec_size, 1024U);
+    config.dynamicSmemBytes = 0;
+    config.stream = stream;
+    cudaLaunchAttribute attrs[1];
+    attrs[0].id = cudaLaunchAttributeProgrammaticStreamSerialization;
+    attrs[0].val.programmaticStreamSerializationAllowed = enable_pdl;
+    config.numAttrs = 1;
+    config.attrs = attrs;
+
+    auto kernel = flashinfer::activation::act_and_mul_kernel<c_type, {{ act_func_name }}>;
+
+    cudaLaunchKernelEx(&config, kernel, static_cast<c_type*>(out->data),
+                       static_cast<c_type*>(input->data), d);
+
+    cudaError_t err = cudaGetLastError();
+    TVM_FFI_ICHECK(err == cudaSuccess) << "Failed to launch kernel: " << cudaGetErrorString(err);
+
+    return true;
+  });
+}
+
+TVM_FFI_DLL_EXPORT_TYPED_FUNC({{ func_name }}, {{ func_name }});
+"""
+
+
+def get_act_and_mul_cu_str(act_func_name: str, act_func_def: str) -> str:
+    template = jinja2.Template(activation_templ)
+    return template.render(act_func_name=act_func_name, act_func_def=act_func_def)
+
+
+def gen_act_and_mul_module(act_func_name: str, act_func_def: str) -> JitSpec:
+    gen_directory = jit_env.FLASHINFER_GEN_SRC_DIR
+    os.makedirs(gen_directory, exist_ok=True)
+    sources = [gen_directory / f"{act_func_name}_and_mul.cu"]
+    write_if_different(
+        sources[0],
+        get_act_and_mul_cu_str(act_func_name, act_func_def),
+    )
+    return gen_jit_spec(
+        f"{act_func_name}_and_mul",
+        sources,
+    )
---
+++ packages/flashinfer-python/flashinfer_python-0.4.0rc4.tar.gz/flashinfer_python-0.4.0rc4/flashinfer/jit/env.py
@@ -0,0 +1,103 @@
+"""
+Copyright (c) 2024 by FlashInfer team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# NOTE(lequn): Do not "from .jit.env import xxx".
+# Do "from .jit import env as jit_env" and use "jit_env.xxx" instead.
+# This helps AOT script to override envs.
+
+import os
+import pathlib
+from ..compilation_context import CompilationContext
+
+FLASHINFER_BASE_DIR = pathlib.Path(
+    os.getenv("FLASHINFER_WORKSPACE_BASE", pathlib.Path.home().as_posix())
+)
+
+FLASHINFER_CACHE_DIR = FLASHINFER_BASE_DIR / ".cache" / "flashinfer"
+
+
+def _get_cubin_dir():
+    """
+    Get the cubin directory path with the following priority:
+    1. flashinfer-cubin package if installed
+    2. Environment variable FLASHINFER_CUBIN_DIR
+    3. Default cache directory
+    """
+    # First check if flashinfer-cubin package is installed
+    try:
+        import flashinfer_cubin
+
+        return pathlib.Path(flashinfer_cubin.get_cubin_dir())
+    except ImportError:
+        pass
+
+    # Then check environment variable
+    env_dir = os.getenv("FLASHINFER_CUBIN_DIR")
+    if env_dir:
+        return pathlib.Path(env_dir)
+
+    # Fall back to default cache directory
+    return FLASHINFER_CACHE_DIR / "cubins"
+
+
+FLASHINFER_CUBIN_DIR = _get_cubin_dir()
+
+
+def _get_workspace_dir_name() -> pathlib.Path:
+    compilation_context = CompilationContext()
+    arch = "_".join(
+        f"{major}{minor}" for major, minor in compilation_context.TARGET_CUDA_ARCHS
+    )
+    return FLASHINFER_CACHE_DIR / arch
+
+
+# use pathlib
+FLASHINFER_WORKSPACE_DIR = _get_workspace_dir_name()
+FLASHINFER_JIT_DIR = FLASHINFER_WORKSPACE_DIR / "cached_ops"
+FLASHINFER_GEN_SRC_DIR = FLASHINFER_WORKSPACE_DIR / "generated"
+_package_root = pathlib.Path(__file__).resolve().parents[1]
+FLASHINFER_DATA = _package_root / "data"
+FLASHINFER_INCLUDE_DIR = _package_root / "data" / "include"
+FLASHINFER_CSRC_DIR = _package_root / "data" / "csrc"
+# FLASHINFER_SRC_DIR = _package_root / "data" / "src"
+FLASHINFER_AOT_DIR = _package_root / "data" / "aot"
+CUTLASS_INCLUDE_DIRS = [
+    _package_root / "data" / "cutlass" / "include",
+    _package_root / "data" / "cutlass" / "tools" / "util" / "include",
+]
+SPDLOG_INCLUDE_DIR = _package_root / "data" / "spdlog" / "include"
+
+
+def get_nvshmem_include_dirs():
+    paths = os.environ.get("NVSHMEM_INCLUDE_PATH")
+    if paths is not None:
+        return [pathlib.Path(p) for p in paths.split(os.pathsep) if p]
+
+    import nvidia.nvshmem
+
+    path = pathlib.Path(nvidia.nvshmem.__path__[0]) / "include"
+    return [path]
+
+
+def get_nvshmem_lib_dirs():
+    paths = os.environ.get("NVSHMEM_LIBRARY_PATH")
+    if paths is not None:
+        return [pathlib.Path(p) for p in paths.split(os.pathsep) if p]
+
+    import nvidia.nvshmem
+
+    path = pathlib.Path(nvidia.nvshmem.__path__[0]) / "lib"
+    return [path]
