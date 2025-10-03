13a88ca3d70b793aa116eee25f013427e8bfcc88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 06:04:48 +0000

    Add package ryseble/1.4.0/ryseble-1.4.0-py3-none-any.whl

---
+++ packages/ryseble/ryseble-1.4.0-py3-none-any.whl/ryseble/constants.py
@@ -0,0 +1,8 @@
+# Hardcoded UUIDs used by Ryse devices
+HARDCODED_UUIDS = {
+    "rx_uuid": "a72f2801-b0bd-498b-b4cd-4a3901388238",
+    "tx_uuid": "a72f2802-b0bd-498b-b4cd-4a3901388238",
+}
+
+# Pairing mode flag
+PAIRING_MODE_FLAG = 0x01
