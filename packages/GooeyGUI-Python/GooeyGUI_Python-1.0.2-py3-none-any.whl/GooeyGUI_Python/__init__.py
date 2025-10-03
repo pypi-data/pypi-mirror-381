7958fb3b456f0c71cbe9ccc5dbb8a72e95b16d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 05:05:22 +0000

    Add package GooeyGUI-Python/1.0.2/GooeyGUI_Python-1.0.2-py3-none-any.whl

---
+++ packages/GooeyGUI-Python/GooeyGUI_Python-1.0.2-py3-none-any.whl/GooeyGUI_Python/__init__.py
@@ -0,0 +1,14 @@
+"""
+GooeyGUI Library
+A Python GUI library ported from C
+"""
+
+from .libgooey import *
+from .gooey_button import *
+from .gooey_canvas import *
+from .gooey_checkbox import *
+from .gooey_dropdown import *
+ # Import other modules as needed
+
+__version__ = "1.0.0"
+__author__ = "BinaryInk"
