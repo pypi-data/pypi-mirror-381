7dce7b21d09f684b0a3e332a590481c0f8672569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 17:04:56 +0000

    Add package valar/1.3.18/valar-1.3.18.tar.gz

---
+++ packages/valar/valar-1.3.18.tar.gz/valar-1.3.18/src/valar/channels/executer.py
@@ -0,0 +1,14 @@
+import asyncio
+
+from ..channels.sender import ValarChannelSender
+
+
+async def execute_channel(method, sender: ValarChannelSender):
+    thread = asyncio.to_thread(__execute__, method, sender)
+    asyncio.create_task(thread)
+
+
+def __execute__(method, sender: ValarChannelSender):
+    sender.start()
+    method(sender)
+    sender.stop()
