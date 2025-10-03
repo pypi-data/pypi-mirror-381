f902d5b97a6cc03c90f238da8b2d9cbe9da14aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 07:05:09 +0000

    Add package helpers-to-python/2.0/helpers_to_python-2.0.tar.gz

---
+++ packages/helpers-to-python/helpers_to_python-2.0.tar.gz/helpers_to_python-2.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='helpers-to-python',
+    version='2.0',
+    author='vova2f',
+    author_email='vovk4756@gmail.com',
+    description='помощники для пайтон',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/vova1f/helpers-to-python',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+    install_requires=[
+        'colorama==0.4.6',
+        'requests==2.32.5'
+    ]
+)
