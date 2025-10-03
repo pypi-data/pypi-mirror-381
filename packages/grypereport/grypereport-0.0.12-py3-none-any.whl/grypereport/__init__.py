3ef08ffb24c5e1e5914dce42874531a526158166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 03:15:04 +0000

    Add package grypereport/0.0.12/grypereport-0.0.12-py3-none-any.whl

---
+++ packages/grypereport/grypereport-0.0.12-py3-none-any.whl/grypereport/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Grype vulnerability scanner report builder and CSV exporter."""
+
+from .report import build
+from .__version__ import version as __version__
+
+__all__ = ["build", "__version__"]
