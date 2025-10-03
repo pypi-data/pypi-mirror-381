063bff65df5a4284b8c24397a70deaa2e7186394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 05:04:38 +0000

    Add package pyssianutils/1.0.2/pyssianutils-1.0.2.tar.gz

---
+++ packages/pyssianutils/pyssianutils-1.0.2.tar.gz/pyssianutils-1.0.2/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+import re
+
+with open('pyssianutils/_version.py','r') as F: 
+    __version__ = re.findall(r'[0-9]*\.[0-9]*\.[0-9]',F.read())[0]
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'pyssianutils',
+  version = __version__,
+  packages = setuptools.find_packages(),
+  description = "A CLI with utils for everyday tasks in CompChem developed with pyssian",
+  author = 'Raúl Pérez-Soto',
+  author_email = 'rperezsoto.research@gmail.com',
+  long_description=long_description,
+  long_description_content_type="text/x-rst",
+  url = 'https://github.com/maserasgroup-repo/pyssian-utils',
+  keywords = ['compchem', 'gaussian','parser'],
+  classifiers = ['Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.12',
+                 ],
+  install_requires=['setuptools',
+                    'pathlib',
+                    'numpy',
+                    'pyssian>=1.1',
+                    'platformdirs'],
+  extras_require={'plotting':['matplotlib','plotly']},
+  python_requires='>=3.10',
+  package_data = {'resources': ['pyssianutils/resources/defaults.ini',
+                                'pyssianutils/resources/templates/slurm/example.txt',
+                                'pyssianutils/resources/templates/slurm/example.json']},
+  include_package_data=True,
+  scripts = ['pyssianutils/pyssianutils'],
+  project_urls={'Bug Reports': 'https://github.com/maserasgroup-repo/pyssian-utils/issues',
+                'Source': 'https://github.com/maserasgroup-repo/pyssian-utils',
+                'Docs' : 'https://maserasgroup-repo.github.io/pyssian-utils/'
+               },
+)
