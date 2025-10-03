8bf460951e41d8ea80f7b20cd3143922848875a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 15:05:37 +0000

    Add package func-validator/0.16.0/func_validator-0.16.0.tar.gz

---
+++ packages/func-validator/func_validator-0.16.0.tar.gz/func_validator-0.16.0/tests/test_decorator.py
@@ -0,0 +1,26 @@
+from typing import Annotated, Optional
+from math import nan
+
+import pytest
+
+from func_validator import (
+    validate_params,
+    DependsOn,
+    ValidationError,
+    MustBePositive,
+)
+
+
+def test_decorator():
+    @validate_params
+    def func(
+            param1: str,
+            param2: Annotated[Optional[str], DependsOn(param1="check")],
+            # default strategy is value must not be empty.
+    ):
+        return param1, param2
+
+    assert func("check", "value") == ("check", "value")
+
+    with pytest.raises(ValidationError):
+        func("check", "")
