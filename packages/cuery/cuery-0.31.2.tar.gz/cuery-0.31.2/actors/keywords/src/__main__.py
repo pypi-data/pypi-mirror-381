052091dafe3d4b46c8f2fa6ebfb2391f9769ed72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 03:14:42 +0000

    Add package cuery/0.31.2/cuery-0.31.2.tar.gz

---
+++ packages/cuery/cuery-0.31.2.tar.gz/cuery-0.31.2/actors/keywords/src/__main__.py
@@ -0,0 +1,5 @@
+import asyncio
+
+from cuery.actors import keywords
+
+asyncio.run(keywords.main())
---
+++ packages/cuery/cuery-0.31.2.tar.gz/cuery-0.31.2/src/cuery/actors/__init__.py
@@ -0,0 +1,5 @@
+"""Apify actors that wrap cuery.seo functionalities."""
+
+from . import keywords, serps, topics, geo
+
+__all__ = ["keywords", "serps", "topics", "geo"]
