e3e14bf18293628df0e31be1193ea19e58184530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 05:05:04 +0000

    Add package ignition-api/8.1.50/ignition_api-8.1.50-py2-none-any.whl

---
+++ packages/ignition-api/ignition_api-8.1.50-py2-none-any.whl/com/inductiveautomation/factorypmi/application/script/builtin/__init__.py
@@ -0,0 +1,462 @@
+from __future__ import print_function
+
+__all__ = [
+    "ClientDatasetUtilities",
+    "ClientSystemUtilities",
+    "INavUtilities",
+    "NavUtilities",
+    "PrintUtilities",
+    "WindowUtilities",
+]
+
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from com.inductiveautomation.factorypmi.application import FPMIApp, FPMIWindow
+from com.inductiveautomation.factorypmi.application.script import PyComponentWrapper
+from com.inductiveautomation.ignition.common.model.values import QualityCode
+from dev.coatl.helper.types import AnyStr
+from java.awt import Color, Component, Graphics
+from java.awt.event import ActionEvent, ComponentEvent, MouseEvent
+from java.awt.image import BufferedImage
+from java.awt.print import PageFormat
+from java.lang import Number, Object
+from java.util import EventObject
+from javax.swing import JComponent, JFrame, JPopupMenu
+from org.python.core import PyObject, PySequence, PyTuple
+
+
+class INavUtilities(object):
+    """Parent interface to coordinate the functions between NavUtilities
+    and NavUtilitiesDispatcher.
+    """
+
+    def centerWindow(self, arg):
+        # type: (Union[FPMIWindow, AnyStr]) -> None
+        raise NotImplementedError
+
+    def closeParentWindow(self, event):
+        # type: (EventObject) -> None
+        raise NotImplementedError
+
+    def closeWindow(self, arg):
+        # type: (Union[FPMIWindow, AnyStr]) -> None
+        raise NotImplementedError
+
+    def getCurrentWindow(self):
+        # type: () -> AnyStr
+        raise NotImplementedError
+
+    def goBack(self):
+        # type: () -> PyObject
+        raise NotImplementedError
+
+    def goForward(self):
+        # type: () -> PyObject
+        raise NotImplementedError
+
+    def goHome(self):
+        # type: () -> PyObject
+        raise NotImplementedError
+
+    def openWindow(self, path, params=None):
+        # type: (AnyStr, Optional[Dict[AnyStr, Any]]) -> PyObject
+        raise NotImplementedError
+
+    def openWindowImpl(self, path, params, openAdditional):
+        # type: (AnyStr, Dict[AnyStr, Any], bool) -> PyObject
+        raise NotImplementedError
+
+    def openWindowInstance(self, path, params=None):
+        # type: (AnyStr, Optional[Dict[AnyStr, Any]]) -> PyObject
+        raise NotImplementedError
+
+    def swapTo(self, name, params):
+        # type: (AnyStr, Dict[AnyStr, Any]) -> PyObject
+        raise NotImplementedError
+
+    def swapWindow(self, *args):
+        # type: (*Any) -> PyObject
+        raise NotImplementedError
+
+
+class ClientDatasetUtilities(Object):
+    def __init__(self, app):
+        # type: (FPMIApp) -> None
+        super(ClientDatasetUtilities, self).__init__()
+        print(app)
+
+
+class ClientSystemUtilities(Object):
+    def __init__(self):
+        # type: () -> None
+        super(ClientSystemUtilities, self).__init__()
+
+
+class NavUtilities(INavUtilities):
+    def centerWindow(self, arg):
+        # type: (Union[FPMIWindow, AnyStr]) -> None
+        pass
+
+    def closeParentWindow(self, event):
+        # type: (EventObject) -> None
+        pass
+
+    def closeWindow(self, arg):
+        # type: (Union[FPMIWindow, AnyStr]) -> None
+        pass
+
+    def getCurrentWindow(self):
+        # type: () -> AnyStr
+        pass
+
+    def goBack(self):
+        # type: () -> PyObject
+        pass
+
+    def goForward(self):
+        # type: () -> PyObject
+        pass
+
+    def goHome(self):
+        # type: () -> PyObject
+        pass
+
+    def openWindow(self, path, params=None):
+        # type: (AnyStr, Optional[Dict[AnyStr, Any]]) -> PyObject
+        pass
+
+    def openWindowImpl(self, path, params, openAdditional):
+        # type: (AnyStr, Dict[AnyStr, Any], bool) -> PyObject
+        pass
+
+    def openWindowInstance(self, path, params=None):
+        # type: (AnyStr, Optional[Dict[AnyStr, Any]]) -> PyObject
+        pass
+
+    def swapTo(self, name, params):
+        # type: (AnyStr, Dict[AnyStr, Any]) -> PyObject
+        pass
+
+    def swapWindow(self, *args):
+        # type: (*Any) -> PyObject
+        pass
+
+
+class PrintUtilities(Object):
+
+    class ComponentPrinter(Object):
+        def __init__(self, c, fit, zoom):
+            # type: (Component, bool, float) -> None
+            super(PrintUtilities.ComponentPrinter, self).__init__()
+            print(c, fit, zoom)
+
+        def print(self, g, pageFormat, pageIndex):
+            # type: (Graphics, PageFormat, int) -> int
+            pass
+
+    class JythonPrintJob(Object):
+        def __init__(self, c):
+            # type: (Component) -> None
+            super(PrintUtilities.JythonPrintJob, self).__init__()
+            print(c)
+
+        def getBottomMargin(self):
+            # type: () -> float
+            pass
+
+        def getLeftMargin(self):
+            # type: () -> float
+            pass
+
+        def getOrientation(self):
+            # type: () -> int
+            pass
+
+        def getPageHeight(self):
+            # type: () -> float
+            pass
+
+        def getPageWidth(self):
+            # type: () -> float
+            pass
+
+        def getPrinterName(self):
+            # type: () -> AnyStr
+            pass
+
+        def getRightMargin(self):
+            # type: () -> float
+            pass
+
+        def getTopMargin(self):
+            # type: () -> float
+            pass
+
+        def getZoomFactor(self):
+            # type: () -> float
+            pass
+
+        def isFitToPage(self):
+            # type: () -> bool
+            return True
+
+        def isShowPrintDialog(self):
+            # type: () -> bool
+            return True
+
+        def setBottomMargin(self, bottomMargin):
+            # type: (float) -> None
+            pass
+
+        def setFitToPage(self, fitToPage):
+            # type: (bool) -> None
+            pass
+
+        def setLeftMargin(self, leftMargin):
+            # type: (float) -> None
+            pass
+
+        def setMargins(self, m):
+            # type: (float) -> None
+            pass
+
+        def setOrientation(self, orientation):
+            # type: (int) -> None
+            pass
+
+        def setPageHeight(self, pageHeight):
+            # type: (float) -> None
+            pass
+
+        def setPageWidth(self, pageWidth):
+            # type: (float) -> None
+            pass
+
+        def setPrinterName(self, printerName):
+            # type: (AnyStr) -> None
+            pass
+
+        def setRightMargin(self, rightMargin):
+            # type: (float) -> None
+            pass
+
+        def setShowPrintDialog(self, showPrintDialog):
+            # type: (bool) -> None
+            pass
+
+        def setZoomFactor(self, zoomFactor):
+            # type: (float) -> None
+            pass
+
+    def __init__(self, app):
+        # type: (Any) -> None
+        super(PrintUtilities, self).__init__()
+        print(self, app)
+
+    def createImage(self, c):
+        # type: (Component) -> BufferedImage
+        print(self, c)
+        width = height = imageType = 1
+        return BufferedImage(width, height, imageType)
+
+    def createPrintJob(self, c):
+        # type: (Component) -> PrintUtilities.JythonPrintJob
+        pass
+
+    def printToImage(self, c, fileName=None):
+        # type: (Component, Optional[str]) -> None
+        pass
+
+
+class WindowUtilities(Object):
+    """These are the scripting functions mounted at system.gui.*.
+
+    Changes to this class must be made carefully, as some of the true
+    implementations actually reside in the subclass,
+    WindowUtilitiesForDesktop.
+    """
+
+    class JyPopupMenu(JPopupMenu):
+        def actionPerformed(self, e):
+            # type: (ActionEvent) -> None
+            pass
+
+        def addJyFunction(self, name, fun):
+            # type: (AnyStr, PyObject) -> None
+            pass
+
+        def show(self, me, *args):
+            # type: (Union[ComponentEvent, MouseEvent], *int) -> None
+            pass
+
+    class PopupContext(Object):
+        def endPopup(self):
+            # type: () -> None
+            pass
+
+        def startPopup(self):
+            # type: () -> None
+            pass
+
+    ACCL_NONE = 0
+    ACCL_CONSTANT = 1
+    ACCL_FAST_TO_SLOW = 2
+    ACCL_SLOW_TO_FAST = 3
+    ACCL_EASE = 4
+    COORD_DESIGNER = 1
+    COORD_SCREEN = 0
+
+    def chooseColor(self, initialColor, dialogTitle="Choose Color"):
+        # type: (Color, Optional[AnyStr]) -> Color
+        pass
+
+    def closeDesktop(self, handle):
+        # type: (AnyStr) -> None
+        pass
+
+    @staticmethod
+    def color(*args):
+        # type: (*Any) -> Color
+        pass
+
+    def confirm(
+        self,
+        message,  # type: AnyStr
+        title="Confirm",  # type: AnyStr
+        allowCancel=False,  # type: bool
+    ):
+        # type: (...) -> Optional[bool]
+        pass
+
+    @staticmethod
+    def convertPointToScreen(x, y, event):
+        # type: (int, int, EventObject) -> Tuple[int, int]
+        pass
+
+    @staticmethod
+    def createPopupContext():
+        # type: () -> WindowUtilities.PopupContext
+        pass
+
+    @staticmethod
+    def createPopupMenu(key, functions):
+        # type: (PySequence, PySequence) -> JPopupMenu
+        pass
+
+    def desktop(self, arg):
+        # type: (Union[int, AnyStr]) -> WindowUtilities
+        pass
+
+    def errorBox(self, message, title="Error"):
+        # type: (AnyStr, Optional[AnyStr]) -> None
+        pass
+
+    @staticmethod
+    def find(component):
+        # type: (JComponent) -> WindowUtilities
+        pass
+
+    def findWindow(self, path):
+        # type: (AnyStr) -> List[PyComponentWrapper]
+        pass
+
+    def getCurrentDesktop(self):
+        # type: () -> AnyStr
+        pass
+
+    def getDesktopHandles(self):
+        # type: () -> PySequence
+        pass
+
+    def getOpenedWindowNames(self):
+        # type: () -> PyTuple
+        pass
+
+    def getOpenedWindows(self):
+        # type: () -> PyTuple
+        pass
+
+    @staticmethod
+    def getParentWindow(event):
+        # type: (EventObject) -> PyObject
+        pass
+
+    def getQuality(self, comp, propertyName):
+        # type: (JComponent, AnyStr) -> QualityCode
+        pass
+
+    def getScreenIndex(self):
+        # type: () -> int
+        pass
+
+    @staticmethod
+    def getScreens():
+        # type: () -> PySequence
+        pass
+
+    @staticmethod
+    def getSibling(event, name):
+        # type: (EventObject, AnyStr) -> PyObject
+        pass
+
+    def getWindow(self, name):
+        # type: (AnyStr) -> PyObject
+        pass
+
+    def getWindowNames(self):
+        # type: () -> PyTuple
+        pass
+
+    def inputBox(self, message, defaultTxt=""):
+        # type: (AnyStr, AnyStr) -> Optional[AnyStr]
+        pass
+
+    def isTouchscreenModeEnabled(self):
+        # type: () -> bool
+        return True
+
+    def messageBox(self, message, title="Information"):
+        # type: (AnyStr, AnyStr) -> None
+        pass
+
+    def openDesktop(self, *args, **kwargs):
+        # type: (*PyObject, **AnyStr) -> JFrame
+        pass
+
+    def openDiagnostics(self):
+        # type: () -> None
+        pass
+
+    def passwordBox(
+        self,
+        message,  # type:AnyStr
+        title="Password",  # type: AnyStr
+        echoChar="*",  # type: AnyStr
+    ):
+        # type: (...) -> Optional[AnyStr]
+        pass
+
+    def setTouchScreenModeEnabled(self, b):
+        # type: (bool) -> None
+        pass
+
+    def showNumericKeyPad(
+        self,
+        initialValue,  # type: Number
+        fontSize=None,  # type: Optional[int]
+        usePasswordMode=False,  # type: bool
+    ):
+        # type: (...) -> Number
+        pass
+
+    def showTouchscreenKeyboard(self, initialText, fontSize=None, password=None):
+        # type: (AnyStr, Optional[int], Optional[bool]) -> AnyStr
+        pass
+
+    def transform(self, *args, **kwargs):
+        # type: (*PyObject, **AnyStr) -> PyObject
+        pass
+
+    def warningBox(self, message, title="Warning"):
+        # type: (AnyStr, AnyStr) -> None
+        pass
---
+++ packages/ignition-api/ignition_api-8.1.50-py2-none-any.whl/system/file.py
@@ -0,0 +1,230 @@
+"""File Functions.
+
+The following functions give you access to read and write to files.
+"""
+
+from __future__ import print_function
+
+__all__ = [
+    "fileExists",
+    "getTempFile",
+    "openFile",
+    "openFiles",
+    "readFileAsBytes",
+    "readFileAsString",
+    "saveFile",
+    "writeFile",
+]
+
+import io
+import os.path
+import tempfile
+from typing import Any, List, Optional
+
+from dev.coatl.helper.types import AnyStr
+
+
+def fileExists(filepath):
+    # type: (AnyStr) -> bool
+    """Checks to see if a file or folder at a given path exists.
+
+    Note:
+        This function is scoped for Perspective Sessions, but since all
+        scripts in Perspective run on the Gateway, the file must be
+        located on the Gateway's file system.
+
+    Args:
+        filepath: The path of the file or folder to check.
+
+    Returns:
+        True if the file/folder exists, False otherwise.
+    """
+    return os.path.isfile(filepath)
+
+
+def getTempFile(extension):
+    # type: (AnyStr) -> AnyStr
+    """Creates a new temp file on the host machine with a certain
+    extension, returning the path to the file.
+
+    The file is marked to be removed when the Java VM exits.
+
+    Note:
+        This function is scoped for Perspective Sessions, but since all
+        scripts in Perspective run on the Gateway, the file must be
+        located on the Gateway's file system.
+
+    Args:
+        extension: An extension, like ".txt", to append to the end of
+            the temporary file.
+
+    Returns:
+        The path to the newly created temp file.
+    """
+    suffix = ".{}".format(extension)
+    with tempfile.NamedTemporaryFile(suffix=suffix) as temp:
+        return unicode(temp.name)
+
+
+def openFile(extension=None, defaultLocation=None):
+    # type: (Optional[AnyStr], Optional[AnyStr]) -> Optional[AnyStr]
+    r"""Shows an "Open File" dialog box, prompting the user to choose a
+    file to open.
+
+    Returns the path to the file that the user chose, or None if the
+    user canceled the dialog box. An extension can optionally be passed
+    in that sets the filetype filter to that extension.
+
+    Args:
+        extension: A file extension, like "pdf", to try to open.
+            Optional.
+        defaultLocation: A folder location, like "C:\\MyFiles", to use
+            as the starting location for the file chooser. Optional.
+
+    Returns:
+        The path to the selected file, or None if canceled.
+    """
+    print(extension, defaultLocation)
+    return ""
+
+
+def openFiles(
+    extension=None,  # type: Optional[AnyStr]
+    defaultLocation=None,  # type: Optional[AnyStr]
+):
+    # type: (...) -> Optional[List[AnyStr]]
+    r"""Shows an "Open File" dialog box, prompting the user to choose a
+    file or files to open.
+
+    Returns the paths to the files that the user chooses, or None if the
+    user canceled the dialog box. An extension can optionally be passed
+    in that sets the filetype filter to that extension.
+
+    Args:
+        extension: A file extension, like "pdf", to try to open.
+            Optional.
+        defaultLocation: A folder location, like "C:\\MyFiles", to use
+            as the starting location for the file chooser. Optional.
+
+    Returns:
+        The paths to the selected files, or None if canceled.
+    """
+    print(extension, defaultLocation)
+    return ["path/to/file"]
+
+
+def readFileAsBytes(filepath):
+    # type: (AnyStr) -> Any
+    """Opens the file found at path filename, and reads the entire file.
+
+    Returns the file as an array of bytes. Commonly this array of bytes
+    is uploaded to a database table with a column of type BLOB (Binary
+    Large OBject). This upload would be done through an INSERT or UPDATE
+    SQL statement run through the system.db.runPrepUpdate function. You
+    could also write the bytes to another file using the
+    system.file.writeFile function, or send the bytes as an email
+    attachment using system.net.sendEmail.
+
+    Note:
+        This function is scoped for Perspective Sessions, but since all
+        scripts in Perspective run on the Gateway, the file must be
+        located on the Gateway's file system.
+
+    Args:
+        filepath: The path of the file to read.
+
+    Returns:
+        The contents of the file as an array of bytes.
+    """
+    with io.open(filepath, "r+b") as f:
+        return f.read()
+
+
+def readFileAsString(filepath, encoding="UTF-8"):
+    # type: (AnyStr, Optional[AnyStr]) -> AnyStr
+    """Opens the file found at path filename, and reads the entire file.
+
+    Returns the file as a string. Common things to do with this string
+    would be to load it into the text property of a component, upload it
+    to a database table, or save it to another file using
+    system.file.writeFile function.
+
+    Note:
+        This function is scoped for Perspective Sessions, but since all
+        scripts in Perspective run on the Gateway, the file must be
+        located on the Gateway's file system.
+
+    Args:
+        filepath: The path of the file to read.
+        encoding: The character encoding of the file to be read. Will
+            throw an exception if the string does not represent a
+            supported encoding. Common encodings are "UTF-8",
+            "ISO-8859-1" and "US-ASCII". Default is "UTF-8". Optional.
+
+    Returns:
+        The contents of the file as a string.
+    """
+    with io.open(filepath, "r", encoding=encoding) as f:
+        return unicode(f.read())
+
+
+def saveFile(
+    filename,  # type: AnyStr
+    extension=None,  # type: Optional[AnyStr]
+    typeDesc=None,  # type: Optional[AnyStr]
+):
+    # type: (...) -> Optional[AnyStr]
+    """Prompts the user to save a new file named filename.
+
+    The optional extension and typeDesc arguments can be added to be
+    used as a type filter. If the user accepts the save, the path to
+    that file will be returned. If the user cancels the save, None will
+    be returned.
+
+    Args:
+        filename: A file name to suggest to the user.
+        extension: The appropriate file extension, like "jpeg", for the
+            file. Optional.
+        typeDesc: A description of the extension, like "JPEG Image".
+            Optional.
+
+    Returns:
+        The path to the file that the user decided to save to, or None
+        if they canceled.
+    """
+    print(filename, extension, typeDesc)
+    return ""
+
+
+def writeFile(
+    filepath,  # type: AnyStr
+    data,  # type: Any
+    append=False,  # type: bool
+    encoding="UTF-8",  # type: AnyStr
+):
+    # type: (...) -> None
+    """Writes the given data to the file at file path filename.
+
+    If the file exists, the append argument determines whether or not it
+    is overwritten (the default) or appended to. The data argument can
+    be either a string or an array of bytes (commonly retrieved from a
+    BLOB in a database or read from another file using
+    system.file.readFileAsBytes).
+
+    Note:
+        This function is scoped for Perspective Sessions, but since all
+        scripts in Perspective run on the Gateway, the file must be
+        located on the Gateway's file system.
+
+    Args:
+        filepath: The path of the file to write to.
+        data: The character or binary content to write to the file.
+        append: If True, the file will be appended to if it already
+            exists. If False, the file will be overwritten if it
+            exists. The default is False. Optional.
+        encoding: The character encoding of the file to write. Will
+            throw an exception if the string does not represent a
+            supported encoding. Common encodings are "UTF-8",
+            "ISO-8859-1" and "US-ASCII". Default is "UTF-8". Optional.
+    """
+    print(filepath, data, append, encoding)
