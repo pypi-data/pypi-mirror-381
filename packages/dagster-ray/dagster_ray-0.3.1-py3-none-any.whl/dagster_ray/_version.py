c22615970dcee10e128b221012e681ac90cd8ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 03:14:59 +0000

    Add package dagster-ray/0.3.1/dagster_ray-0.3.1-py3-none-any.whl

---
+++ packages/dagster-ray/dagster_ray-0.3.1-py3-none-any.whl/dagster_ray/_version.py
@@ -0,0 +1,1 @@
+__version__ = "0.3.1"  # managed by hatch
---
+++ packages/dagster-ray/dagster_ray-0.3.1-py3-none-any.whl/dagster_ray/kuberay/jobs.py
@@ -0,0 +1,16 @@
+import dagster as dg
+
+from dagster_ray.kuberay.ops import cleanup_kuberay_clusters_op, delete_kuberay_clusters_op
+
+
+@dg.job(description="Deletes RayCluster resources from Kubernetes", name="delete_kuberay_rayclusters")
+def delete_kuberay_clusters():
+    delete_kuberay_clusters_op()
+
+
+@dg.job(
+    description="Deletes RayCluster resources which do not correspond to any active Dagster Runs in this deployment from Kubernetes",
+    name="cleanup_kuberay_rayclusters",
+)
+def cleanup_kuberay_clusters():
+    cleanup_kuberay_clusters_op()
