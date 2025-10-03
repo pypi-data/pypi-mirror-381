a3dae4af3d59b6b97a3ed595778035b02bfdf6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 15:06:47 +0000

    Add package wisdem/3.22.5/wisdem-3.22.5-cp313-cp313-win_amd64.whl

---
+++ packages/wisdem/wisdem-3.22.5-cp313-cp313-win_amd64.whl/wisdem/inputs/validation.py
@@ -0,0 +1,344 @@
+import os
+import copy
+import numpy as np
+import jsonschema as json
+import ruamel.yaml as ry
+from functools import reduce
+import operator
+from openmdao.utils.mpi import MPI
+
+
+fschema_geom = os.path.join(os.path.dirname(os.path.realpath(__file__)), "geometry_schema.yaml")
+fschema_model = os.path.join(os.path.dirname(os.path.realpath(__file__)), "modeling_schema.yaml")
+fschema_opt = os.path.join(os.path.dirname(os.path.realpath(__file__)), "analysis_schema.yaml")
+
+
+def load_yaml(fname_input : str) -> dict:
+    """
+    Reads and parses a YAML file in a safe mode using the ruamel.yaml library.
+
+    Args:
+        fname_input (str): Path to the YAML file to be loaded.
+
+    Returns:
+        dict: Parsed YAML content as a dictionary.
+    """
+    reader = ry.YAML(typ="safe", pure=True)
+    with open(fname_input, "r", encoding="utf-8") as f:
+        input_yaml = reader.load(f)
+    return input_yaml
+
+def write_yaml(instance : dict, foutput : str) -> None:
+    """
+    Writes a dictionary to a YAML file using the ruamel.yaml library.
+
+    Args:
+        instance (dict): Dictionary to be written to the YAML file.
+        foutput (str): Path to the output YAML file.
+
+    Returns:
+        None
+    """
+    instance = remove_numpy(instance)
+
+    # Write yaml with updated values
+    yaml = ry.YAML()
+    yaml.default_flow_style = None
+    yaml.width = float("inf")
+    yaml.indent(mapping=4, sequence=6, offset=3)
+    yaml.allow_unicode = False
+    rank = MPI.COMM_WORLD.Get_rank() if MPI else 0
+    if rank == 0:
+        with open(foutput, "w", encoding="utf-8") as f:
+            yaml.dump(instance, f)
+
+
+# ---------------------
+# This is for when the defaults are in another file
+def nested_get(indict, keylist):
+    rv = indict
+    for k in keylist:
+        rv = rv[k]
+    return rv
+
+
+def nested_set(indict, keylist, val):
+    rv = indict
+    for i, k in enumerate(keylist):
+        rv = rv[k] if i != len(keylist) - 1 else val
+
+
+def integrate_defaults(instance : dict, defaults : dict, yaml_schema : dict) -> dict:
+    """
+    Integrates default values from a dictionary into another dictionary.
+
+    Args:
+        instance (dict): Dictionary to be updated with default values.
+        defaults (dict): Dictionary containing default values.
+        yaml_schema (dict): Dictionary containing the schema of the YAML file.
+
+    Returns:
+        dict: Updated dictionary with default values integrated.
+    """
+    # Prep iterative validator
+    # json.validate(self.wt_init, yaml_schema)
+    validator = json.Draft7Validator(yaml_schema)
+    errors = validator.iter_errors(instance)
+
+    # Loop over errors
+    for e in errors:
+        # If the error is due to a missing required value, try to set it to the default
+        if e.validator == "required":
+            for k in e.validator_value:
+                if k not in e.instance.keys():
+                    mypath = e.absolute_path.copy()
+                    mypath.append(k)
+                    v = nested_get(defaults, mypath)
+                    if isinstance(v, dict) or isinstance(v, list) or v in ["name", "material"]:
+                        # Too complicated to just copy over default, so give it back to the user
+                        raise (e)
+                    print("WARNING: Missing value,", list(mypath), ", so setting to:", v)
+                    nested_set(instance, mypath, v)
+        raise (e)
+    return instance
+
+
+def simple_types(indict : dict) -> dict:
+    """
+    Recursively converts numpy array elements within a nested dictionary to lists and ensures
+    all values are simple types (float, int, dict, bool, str).
+
+    Args:
+        indict (dict): The dictionary to process.
+
+    Returns:
+        dict: The processed dictionary with numpy arrays converted to lists and unsupported types to empty strings.
+    """
+    def convert(value):
+        if isinstance(value, np.ndarray):
+            return convert(value.tolist())
+        elif isinstance(value, dict):
+            return {key: convert(value) for key, value in value.items()}
+        elif isinstance(value, (list, tuple, set)):
+            return [convert(item) for item in value]  # treat all as list
+        elif isinstance(value, (np.generic)):
+            return value.item()  # convert numpy primatives to python primative underlying
+        elif isinstance(value, (float, int, bool, str)):
+            return value  # this should be the end case
+        else:
+            return ""
+    return convert(indict)
+
+
+# ---------------------
+# See: https://python-jsonschema.readthedocs.io/en/stable/faq/#why-doesn-t-my-schema-s-default-property-set-the-default-on-my-instance
+def extend_with_default(validator_class):
+    validate_properties = validator_class.VALIDATORS["properties"]
+
+    def set_defaults(validator, properties, instance, schema):
+        for property, subschema in properties.items():
+            if "default" in subschema:
+                instance.setdefault(property, subschema["default"])
+
+        for error in validate_properties(validator, properties, instance, schema):
+            yield error
+
+    return json.validators.extend(validator_class, {"properties": set_defaults})
+
+
+DefaultValidatingDraft7Validator = extend_with_default(json.Draft7Validator)
+
+def MPI_load_yaml(fname):
+    """
+    When MPI is active, loads a yaml on rank 0 and broadcasts it out
+
+    Args:
+        fname: file name of input yaml file
+
+    Returns:
+        dict: Dictionary corresponding to that yaml file
+    """
+
+    rank = MPI.COMM_WORLD.Get_rank()
+    dict_yaml = load_yaml(fname) if rank == 0 else None
+    dict_yaml = MPI.COMM_WORLD.bcast(dict_yaml, root = 0)
+
+    return dict_yaml
+
+def _validate(finput, fschema, defaults=True, rank_0 = False):
+    """
+    Validates a dictionary against a schema and returns the validated dictionary.
+
+    Args:
+        finput (dict or str): Dictionary or path to the YAML file to be validated.
+        fschema (dict or str): Dictionary or path to the schema file to validate against.
+        defaults (bool): Flag to indicate if default values should be integrated.
+        rank_0 (bool): Flag that should be set to true when the _validate function is
+        called with MPI turned on and rank=0. Otherwise it can be kept as default to False
+
+    Returns:
+        dict: Validated dictionary.
+    """
+    if isinstance(fschema, dict):
+        schema_dict = fschema
+    else:
+        schema_dict = MPI_load_yaml(fschema) if (MPI and rank_0 == False) else load_yaml(fschema)
+
+    
+    if isinstance(finput, dict):
+        input_dict = finput
+    else:
+        input_dict = MPI_load_yaml(finput) if (MPI and rank_0 == False) else load_yaml(finput)
+
+    # schema_dict = fschema if isinstance(fschema, dict) else load_yaml(fschema)
+    # input_dict = finput if isinstance(finput, dict) else load_yaml(finput)
+    validator = DefaultValidatingDraft7Validator if defaults else json.Draft7Validator
+    if MPI and rank_0 == 0:
+        validator(schema_dict).validate(input_dict)
+    elif not MPI:
+        validator(schema_dict).validate(input_dict)
+
+    # Deep copy to ensure no shared references from yaml pointers and anchors
+    unique_input_dict = deep_copy_without_shared_refs(input_dict)
+
+    return unique_input_dict
+
+def deep_copy_without_shared_refs(obj):
+    """
+    Recursively creates a deep copy of the object, breaking any shared references.
+    """
+    if isinstance(obj, dict):
+        return {k: deep_copy_without_shared_refs(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [deep_copy_without_shared_refs(item) for item in obj]
+    else:
+        return obj  # Return the value directly if not a container
+
+# ---------------------
+def load_geometry_yaml(finput):
+    return _validate(finput, fschema_geom)
+
+
+def load_modeling_yaml(finput):
+    return _validate(finput, fschema_model)
+
+
+def load_analysis_yaml(finput):
+    return _validate(finput, fschema_opt)
+
+
+def write_geometry_yaml(instance : dict, foutput : str) -> None:
+    _validate(instance, fschema_geom, defaults=False, rank_0=True)
+    # Ensure the file output has a .yaml suffix
+    if not foutput.endswith(".yaml"):
+        foutput += ".yaml"
+    write_yaml(instance, foutput)
+    return foutput
+
+
+def write_modeling_yaml(instance : dict, foutput : str) -> None:
+    _validate(instance, fschema_model, defaults=False, rank_0=True)
+
+    # Ensure the output filename does not end with .yaml or .yml
+    if foutput.endswith(".yaml"):
+        foutput = foutput[:-5]
+    elif foutput.endswith(".yml"):
+        foutput = foutput[:-4]
+    sfx_str = "-modeling.yaml"
+
+    instance2 = simple_types(instance)
+    write_yaml(instance2, foutput + sfx_str)
+    return foutput + sfx_str
+
+
+def write_analysis_yaml(instance : dict, foutput : str) -> None:
+    _validate(instance, fschema_opt, defaults=False, rank_0=True)
+
+    # Ensure the output filename does not end with .yaml or .yml
+    if foutput.endswith(".yaml"):
+        foutput = foutput[:-5]
+    elif foutput.endswith(".yml"):
+        foutput = foutput[:-4]
+
+    sfx_str = "-analysis.yaml"
+    write_yaml(instance, foutput + sfx_str)
+    return foutput + sfx_str
+
+def remove_numpy(fst_vt : dict) -> dict:
+    """
+    Recursively converts numpy array elements within a nested dictionary to lists and ensures
+    all values are simple types (float, int, dict, bool, str) for writing to a YAML file.
+
+    Args:
+        fst_vt (dict): The dictionary to process.
+
+    Returns:
+        dict: The processed dictionary with numpy arrays converted to lists and unsupported types to simple types.
+    """
+
+    def get_dict(vartree, branch):
+        return reduce(operator.getitem, branch, vartree)
+
+    # Define conversion dictionary for numpy types
+    conversions = {
+        np.int_: int,
+        np.intc: int,
+        np.intp: int,
+        np.int8: int,
+        np.int16: int,
+        np.int32: int,
+        np.int64: int,
+        np.uint8: int,
+        np.uint16: int,
+        np.uint32: int,
+        np.uint64: int,
+        np.single: float,
+        np.double: float,
+        np.longdouble: float,
+        np.csingle: float,
+        np.cdouble: float,
+        np.float16: float,
+        np.float32: float,
+        np.float64: float,
+        np.complex64: float,
+        np.complex128: float,
+        np.bool_: bool,
+        np.ndarray: lambda x: x.tolist(),
+    }
+
+    def loop_dict(vartree, branch):
+        if not isinstance(vartree, dict):
+            return fst_vt
+        for var in vartree.keys():
+            branch_i = copy.copy(branch)
+            branch_i.append(var)
+            if isinstance(vartree[var], dict):
+                loop_dict(vartree[var], branch_i)
+            else:
+                current_value = get_dict(fst_vt, branch_i[:-1])[branch_i[-1]]
+                data_type = type(current_value)
+                if data_type in conversions:
+                    get_dict(fst_vt, branch_i[:-1])[branch_i[-1]] = conversions[data_type](current_value)
+                elif isinstance(current_value, (list, tuple)):
+                    for i, item in enumerate(current_value):
+                        current_value[i] = remove_numpy(item)
+
+    # set fast variables to update values
+    loop_dict(fst_vt, [])
+    return fst_vt
+
+
+if __name__ == "__main__":
+    yaml_schema = load_yaml(fschema_opt)
+    myobj = load_yaml("sample_analysis.yaml")
+    DefaultValidatingDraft7Validator(yaml_schema).validate(myobj)
+    # validator.validate( myobj )
+    print([k for k in myobj.keys()])
+    print(myobj["general"])
+
+    obj = {}
+    schema = {"properties": {"foo": {"default": "bar"}}}
+    # Note jsonschem.validate(obj, schema, cls=DefaultValidatingDraft7Validator)
+    # will not work because the metaschema contains `default` directives.
+    DefaultValidatingDraft7Validator(schema).validate(obj)
+    print(obj)
---
+++ packages/wisdem/wisdem-3.22.5-cp313-cp313-win_amd64.whl/wisdem/test/test_gluecode/test_gluecode.py
@@ -0,0 +1,55 @@
+import os
+import unittest
+
+from wisdem.glue_code.runWISDEM import run_wisdem
+
+test_dir = (
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+    + os.sep
+    + "examples"
+    + os.sep
+    + "02_reference_turbines"
+    + os.sep
+)
+fname_modeling_options = test_dir + "modeling_options.yaml"
+fname_analysis_options = test_dir + "analysis_options.yaml"
+
+
+class TestRegression(unittest.TestCase):
+    def test5MW(self):
+        ## NREL 5MW
+        fname_wt_input = test_dir + "nrel5mw.yaml"
+
+        wt_opt, _, _ = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
+
+        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 23.65765001618882, 2)
+        self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 16419.8666989212, 2)
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3,52.67452671974736, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 4.458378462584996, 1)
+        self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 87.7, 2)
+
+    def test15MW(self):
+        ## IEA 15MW
+        fname_wt_input = test_dir + "IEA-15-240-RWT.yaml"
+        wt_opt, _, _ = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
+
+        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 77.9000397734597, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 68208.64259485099, -1)
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 75.38160410349971, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 25.93986529952085, 1)
+        self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 144.386, 3)
+
+    def test3p4MW(self):
+        ## IEA 3.4MW
+        fname_wt_input = test_dir + "IEA-3p4-130-RWT.yaml"
+        wt_opt, _, _ = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
+
+        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 13.586883417647216, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 14528.445083090577, 1)
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 38.313140385601535, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 8.10488877465953, 1)
+        self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 108.0, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
