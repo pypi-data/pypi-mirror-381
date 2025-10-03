009e29eca96a1aa96e7c7830ff3d8d0b3037e707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-03 05:05:25 +0000

    Add package shudaodao/0.1.36/shudaodao-0.1.36-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/shudaodao/shudaodao-0.1.36-cp310-cp310-macosx_10_9_x86_64.whl/shudaodao_core/config/schemas/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @License  ：(C)Copyright 2025, 数道智融科技
+# @Author   ：李锋
+# @Software ：PyCharm
+# @Date     ：2025/9/21 下午8:47
+# @Desc     ：
+
+
+from pydantic import BaseModel, Field
+
+
+class AuthConfigSetting(BaseModel):
+    token_jwt_secret_key: str = Field(..., description="JWT TOKEN_SECRET_KEY")
+    token_jwt_expire_minutes: int = Field(30, description="JWT TOKEN_ 过期时间")
+    rebuild_auth_rule: bool = Field(False, description="重置")
+    default_admin_roles: list[str] = ["admin"]
+    default_admin_users: list[str] = ["admin"]
