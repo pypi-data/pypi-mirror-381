37ad7531b0715a64d0050732d8c57647e615755e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 14:06:13 +0000

    Add package paicorelib/1.5.0a3/paicorelib-1.5.0a3-py3-none-any.whl

---
+++ packages/paicorelib/paicorelib-1.5.0a3-py3-none-any.whl/paicorelib/framelib/types.py
@@ -0,0 +1,26 @@
+import sys
+from typing import TypeVar
+
+import numpy as np
+from numpy.typing import NDArray
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
+FRAME_DTYPE: TypeAlias = np.uint64
+FrameArrayType: TypeAlias = NDArray[FRAME_DTYPE]
+PAYLOAD_DATA_DTYPE: TypeAlias = np.uint8  # in work frame type I
+PayloadDataType: TypeAlias = NDArray[PAYLOAD_DATA_DTYPE]
+LUT_DTYPE: TypeAlias = np.int8
+LUTDataType: TypeAlias = NDArray[LUT_DTYPE]
+
+BasicFrameArray = TypeVar(
+    "BasicFrameArray", int, list[int], tuple[int, ...], NDArray[FRAME_DTYPE]
+)
+IntScalarType = TypeVar("IntScalarType", int, np.bool, np.integer)
+DataType = TypeVar("DataType", int, np.bool, np.integer, np.ndarray)
+DataArrayType = TypeVar(
+    "DataArrayType", int, np.bool, np.integer, list[int], tuple[int, ...], np.ndarray
+)
