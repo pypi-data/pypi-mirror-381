04904b9922e5124fd0cc1816ad14a926d3d757bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 06:06:32 +0000

    Add package open-actuator/0.1.0/open_actuator-0.1.0-py3-none-any.whl

---
+++ packages/open-actuator/open_actuator-0.1.0-py3-none-any.whl/open_actuator/__init__.py
@@ -0,0 +1,9 @@
+"""
+Open Actuator - A Python library for controlling actuators.
+
+This package is currently under development.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Paul Hetherington"
+__email__ = "contact@hlaboratories.com"
