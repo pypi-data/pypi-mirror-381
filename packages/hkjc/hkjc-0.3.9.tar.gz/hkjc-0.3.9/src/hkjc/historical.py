d85df20ff4c771254ad1668c29a75e83cd646be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 15:06:19 +0000

    Add package hkjc/0.3.9/hkjc-0.3.9.tar.gz

---
+++ packages/hkjc/hkjc-0.3.9.tar.gz/hkjc-0.3.9/src/hkjc/historical.py
@@ -0,0 +1,13 @@
+"""Functions to fetch and process historical race and horse data from HKJC
+"""
+from __future__ import annotations
+
+import requests
+
+# TODO read and process all races from start date to end date
+
+# TODO query all basic info and race history for a specific horse
+
+# TODO classify running style & draw to determine blocking probability
+
+# 
