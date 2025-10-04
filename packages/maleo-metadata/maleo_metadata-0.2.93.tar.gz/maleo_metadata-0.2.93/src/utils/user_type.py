3f9ef7cd7b60b0d02d1c5640df2b8cd094351212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 06:05:41 +0000

    Add package maleo-metadata/0.2.93/maleo_metadata-0.2.93.tar.gz

---
+++ packages/maleo-metadata/maleo_metadata-0.2.93.tar.gz/maleo_metadata-0.2.93/src/utils/user_type.py
@@ -0,0 +1,35 @@
+from typing import Literal, Type, overload
+from ..dtos.user_type import (
+    BasicUserTypeData,
+    StandardUserTypeData,
+    FullUserTypeData,
+    AnyUserTypeDataType,
+)
+from ..enums.user_type import Granularity
+
+
+@overload
+def get_data_model(
+    granularity: Literal[Granularity.BASIC],
+    /,
+) -> Type[BasicUserTypeData]: ...
+@overload
+def get_data_model(
+    granularity: Literal[Granularity.STANDARD],
+    /,
+) -> Type[StandardUserTypeData]: ...
+@overload
+def get_data_model(
+    granularity: Literal[Granularity.FULL],
+    /,
+) -> Type[FullUserTypeData]: ...
+def get_data_model(
+    granularity: Granularity,
+    /,
+) -> AnyUserTypeDataType:
+    if granularity is Granularity.BASIC:
+        return BasicUserTypeData
+    elif granularity is Granularity.STANDARD:
+        return StandardUserTypeData
+    elif granularity is Granularity.FULL:
+        return FullUserTypeData
