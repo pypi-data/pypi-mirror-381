c580f854f35e226fcbc3f3ecad3f2498dadd53c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 15:06:05 +0000

    Add package ilbuilder/0.1.0/ilbuilder-0.1.0-py3-none-any.whl

---
+++ packages/ilbuilder/ilbuilder-0.1.0-py3-none-any.whl/ilbuilder_cli/build.py
@@ -0,0 +1,12 @@
+﻿from ilbuilder.builder.builder import IlBuilder
+
+
+class TestBuilder(IlBuilder):
+    def __init__(self, name, script, icon=None):
+        super().__init__(name, script, icon)
+
+        self._console = True
+
+    @staticmethod
+    def from_data(root: str) -> 'IlBuilder':
+        pass
