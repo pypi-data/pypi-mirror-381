aa84ae45a6846b7f580b65ccebc57694484e4ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 04:05:36 +0000

    Add package Invenio-Theme-TUW/2025.2.8/invenio_theme_tuw-2025.2.8.tar.gz

---
+++ packages/Invenio-Theme-TUW/invenio_theme_tuw-2025.2.8.tar.gz/invenio_theme_tuw-2025.2.8/invenio_theme_tuw/theme/assets/semantic-ui/js/invenio_theme_tuw/mobilemenu/index.js
@@ -0,0 +1,17 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2021 TU Wien.
+//
+// Invenio Theme TUW is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import $ from "jquery";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("nav-icon").addEventListener("click", function () {
+    $(this).toggleClass("open");
+    $("#sidebar").sidebar("setting", "onHide", () => {
+      $("#nav-icon").removeClass("open");
+    });
+    $("#sidebar").sidebar("show");
+  });
+});
