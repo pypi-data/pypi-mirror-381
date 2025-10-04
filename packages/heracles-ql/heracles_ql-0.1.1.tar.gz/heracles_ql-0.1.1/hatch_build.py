299d8fe5fb8d070d48d7349f3d9b68105d342538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 05:05:57 +0000

    Add package heracles-ql/0.1.1/heracles_ql-0.1.1.tar.gz

---
+++ packages/heracles-ql/heracles_ql-0.1.1.tar.gz/heracles_ql-0.1.1/hatch_build.py
@@ -0,0 +1,21 @@
+import subprocess
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+class BuildSharedObjectd(BuildHookInterface):
+    def initialize(self, version, build_data):
+        build_data["pure_python"] = False
+
+        subprocess.check_output(
+            [
+                "go",
+                "build",
+                "-C",
+                "formatter",
+                "-buildmode=c-shared",
+                "-o",
+                "../pkg/formatter.so",
+                "formatter.go",
+            ]
+        )
