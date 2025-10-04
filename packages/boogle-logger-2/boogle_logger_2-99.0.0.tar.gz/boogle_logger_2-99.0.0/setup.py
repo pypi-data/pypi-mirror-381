9a86211badbe6128e2d216f5a415d5ac1d9bec94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 04:05:46 +0000

    Add package boogle-logger-2/99.0.0/boogle_logger_2-99.0.0.tar.gz

---
+++ packages/boogle-logger-2/boogle_logger_2-99.0.0.tar.gz/boogle_logger_2-99.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="boogle_logger_2",
+    version="99.0.0",
+    description="Dependency Confusion Attack Demo",
+    author="Attacker",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[],
+)
+
