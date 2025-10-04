7ba5acea0d1b4d2e9f1cfdbc61321b7e01c4800c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 07:05:01 +0000

    Add package orb-billing/4.22.0/orb_billing-4.22.0-py3-none-any.whl

---
+++ packages/orb-billing/orb_billing-4.22.0-py3-none-any.whl/orb/types/item.py
@@ -0,0 +1,35 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Dict, List
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["Item", "ExternalConnection"]
+
+
+class ExternalConnection(BaseModel):
+    external_connection_name: Literal[
+        "stripe", "quickbooks", "bill.com", "netsuite", "taxjar", "avalara", "anrok", "numeral"
+    ]
+
+    external_entity_id: str
+
+
+class Item(BaseModel):
+    id: str
+
+    created_at: datetime
+
+    external_connections: List[ExternalConnection]
+
+    metadata: Dict[str, str]
+    """User specified key-value pairs for the resource.
+
+    If not present, this defaults to an empty dictionary. Individual keys can be
+    removed by setting the value to `null`, and the entire metadata mapping can be
+    cleared by setting `metadata` to `null`.
+    """
+
+    name: str
