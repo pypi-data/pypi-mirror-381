f8ebbcd650d4428695c2645fbac15ae9759cb4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 05:05:40 +0000

    Add package qaz008fear/0.2/qaz008fear-0.2.tar.gz

---
+++ packages/qaz008fear/qaz008fear-0.2.tar.gz/qaz008fear-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='qaz008fear', # Nombre de tu paquete
+    packages=find_packages(),
+    version='0.2', # Versión de tu paquete
+    description='geoestadistica',
+    author='fede',
+    author_email='fededimant@hotmail.com',
+    url='https://github.com/fededimant/qaz008fear', # URL del repositorio
+    # Encuentra automáticamente los paquetes
+  
+)
