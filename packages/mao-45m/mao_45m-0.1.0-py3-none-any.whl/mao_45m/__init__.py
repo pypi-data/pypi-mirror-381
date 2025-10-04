0beb22e1542d82a35717b475c28296f6ebf80e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 14:06:30 +0000

    Add package mao-45m/0.1.0/mao_45m-0.1.0-py3-none-any.whl

---
+++ packages/mao-45m/mao_45m-0.1.0-py3-none-any.whl/mao_45m/__init__.py
@@ -0,0 +1,27 @@
+__all__ = ["cosmos", "subref", "vdif", "utils"]
+
+
+# dependencies
+from fire import Fire
+from . import cosmos
+from . import subref
+from . import vdif
+from . import utils
+
+
+def main():
+    Fire(
+        {
+            "cosmos": {
+                "receive": cosmos.receive,
+                "send": cosmos.send,
+            },
+            "subref": {
+                "control": subref.control.control,
+            },
+            "vdif": {
+                "receive": vdif.receive.receive,
+                "send": vdif.send.send,
+            },
+        }
+    )
