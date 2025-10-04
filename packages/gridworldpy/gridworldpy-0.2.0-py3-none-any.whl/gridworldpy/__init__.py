f1f67e54fb4d1b04a2dc7a27b8fd6774fc082a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 15:06:20 +0000

    Add package gridworldpy/0.2.0/gridworldpy-0.2.0-py3-none-any.whl

---
+++ packages/gridworldpy/gridworldpy-0.2.0-py3-none-any.whl/gridworldpy/__init__.py
@@ -0,0 +1,16 @@
+"""
+GridWorldPy - A Python package for creating and visualizing grid world environments.
+
+This package provides a flexible and interactive grid world environment
+for reinforcement learning experiments and educational purposes.
+"""
+
+from .gridworld import GridWorldEnv
+from .utils import policy_to_transition_matrix, matrix_to_rewards
+from .q_table import QTable
+
+__version__ = "0.2.0"
+__author__ = "LIC"
+__email__ = "liuchen.lic@gmail.com"
+
+__all__ = ["GridWorldEnv", "policy_to_transition_matrix", "matrix_to_rewards", "QTable"]
