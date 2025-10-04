622adbf70b1acc8ff3021271c7a25d3c563453b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 15:05:59 +0000

    Add package hybridoma/0.2.1/hybridoma-0.2.1.tar.gz

---
+++ packages/hybridoma/hybridoma-0.2.1.tar.gz/hybridoma-0.2.1/hybridoma/__init__.py
@@ -0,0 +1,22 @@
+from .hybridoma import (
+    App,
+    db,
+    ViewModel,
+    HyDB,
+    view_model
+)
+from quart import render_template
+from quart import (
+    request,
+    jsonify,
+    session,
+    g,
+    abort,
+    redirect,
+    url_for,
+    flash,
+)
+
+from . import quart
+
+quart.render_template = render_template = App.render
