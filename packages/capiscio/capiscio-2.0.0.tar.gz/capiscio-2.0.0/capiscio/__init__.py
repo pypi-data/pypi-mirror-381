292a9c90e86c4d971e9d9b3b48fd85cf507fbf42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 05:05:39 +0000

    Add package capiscio/2.0.0/capiscio-2.0.0.tar.gz

---
+++ packages/capiscio/capiscio-2.0.0.tar.gz/capiscio-2.0.0/capiscio/__init__.py
@@ -0,0 +1,10 @@
+"""
+Capiscio - A2A Protocol Validator and CLI Tool
+
+This package provides a Python wrapper around the Capiscio CLI tool,
+allowing easy installation and usage via pip.
+"""
+
+__version__ = "2.0.0"
+__author__ = "Capiscio Team"
+__email__ = "hello@capiscio.com"
