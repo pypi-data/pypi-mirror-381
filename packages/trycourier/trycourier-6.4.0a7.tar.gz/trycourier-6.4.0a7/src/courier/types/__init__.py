1437826ad50b23ef23a87768ba342a68fd1590e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 06:05:55 +0000

    Add package trycourier/6.4.0a7/trycourier-6.4.0a7.tar.gz

---
+++ packages/trycourier/trycourier-6.4.0a7.tar.gz/trycourier-6.4.0a7/src/courier/types/__init__.py
@@ -0,0 +1,12 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .utm_param import UtmParam as UtmParam
+from .recipient_param import RecipientParam as RecipientParam
+from .preference_param import PreferenceParam as PreferenceParam
+from .send_message_params import SendMessageParams as SendMessageParams
+from .message_context_param import MessageContextParam as MessageContextParam
+from .message_routing_param import MessageRoutingParam as MessageRoutingParam
+from .send_message_response import SendMessageResponse as SendMessageResponse
+from .message_routing_channel_param import MessageRoutingChannelParam as MessageRoutingChannelParam
