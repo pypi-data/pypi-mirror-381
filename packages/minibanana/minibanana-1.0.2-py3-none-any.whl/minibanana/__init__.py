c27bff855964ba1cce228e42439f564be33b2de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 06:06:24 +0000

    Add package minibanana/1.0.2/minibanana-1.0.2-py3-none-any.whl

---
+++ packages/minibanana/minibanana-1.0.2-py3-none-any.whl/minibanana/__init__.py
@@ -0,0 +1,25 @@
+from core import (
+    window,
+    draw_rect,
+    draw_sprite,
+    draw_text,
+    get_keys,
+    colliderect,
+    play_sound,
+    stop_sound,
+    start,
+    set_logic
+)
+
+__all__ = [
+    "window",
+    "draw_rect",
+    "draw_sprite",
+    "draw_text",
+    "get_keys",
+    "colliderect",
+    "play_sound",
+    "stop_sound",
+    "start",
+    "set_logic",
+]
