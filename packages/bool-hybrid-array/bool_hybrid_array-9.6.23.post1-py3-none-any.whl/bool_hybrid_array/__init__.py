a3db1a8e6843779ab0b426266a359056d7ca2e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 15:06:19 +0000

    Add package bool-hybrid-array/9.6.23.post1/bool_hybrid_array-9.6.23.post1-py3-none-any.whl

---
+++ packages/bool-hybrid-array/bool_hybrid_array-9.6.23.post1-py3-none-any.whl/bool_hybrid_array/__init__.py
@@ -0,0 +1,31 @@
+import sys
+from types import ModuleType
+from . import core
+from .core import (BHA_Bool, BHA_List, BoolHybridArray, BoolHybridArr,
+                                TruesArray, FalsesArray, ProtectedBuiltinsDict, builtins,
+                                __builtins__,ResurrectMeta,BHA_Iterator,BHA_bool)
+__version__ = "9.6.23.post1"
+try:
+    original_builtins_dict = builtins.copy()
+    __builtins__ = ProtectedBuiltinsDict(original_builtins_dict)
+    builtins = __builtins__
+except Exception:
+    pass
+try:
+    __dict__ = ProtectedBuiltinsDict(globals())
+    sys.modules[__name__] = ProtectedBuiltinsDict(globals())
+    core.__dict__ = ProtectedBuiltinsDict(core.__dict__)
+except Exception as e:
+    pass
+public_objects = []
+for name in dir(core):
+    if not name.startswith("_"):
+        obj = getattr(core, name)
+        if isinstance(obj, (type, ModuleType)) or callable(obj):
+            public_objects.append(name)
+__all__ = public_objects + ["__version__","__builtins__","core","builtins","__dict__"]
+globals().update({
+    name: getattr(core, name)
+    for name in public_objects
+})
+
