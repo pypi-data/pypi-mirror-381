28ad64a690eeb58914ce654c062478a118584bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 05:05:25 +0000

    Add package bluer-ugv/6.1102.1/bluer_ugv-6.1102.1-py3-none-any.whl

---
+++ packages/bluer-ugv/bluer_ugv-6.1102.1-py3-none-any.whl/bluer_ugv/README/UGVs.py
@@ -0,0 +1,48 @@
+from bluer_objects.README.items import ImageItems
+
+docs = [
+    {
+        "path": "../docs/UGVs",
+    },
+    {
+        "path": "../docs/UGVs/swallow.md",
+        "items": ImageItems(
+            {
+                "https://github.com/kamangir/assets2/raw/main/bluer-swallow/20250701_2206342_1.gif?raw=true": "",
+                "https://github.com/kamangir/assets2/raw/main/bluer-swallow/20250913_203635~2_1.gif?raw=true": "",
+            }
+        ),
+    },
+    {
+        "path": "../docs/UGVs/sparrow.md",
+        "items": ImageItems(
+            {
+                "https://github.com/kamangir/assets2/raw/main/bluer-sparrow/VID-20250905-WA0014_1.gif?raw=true": "",
+            }
+        ),
+    },
+    {
+        "path": "../docs/UGVs/sparrow2.md",
+        "items": ImageItems(
+            {
+                "https://github.com/kamangir/assets2/raw/main/bluer-sparrow/VID-20250905-WA0014_1.gif?raw=true": "",
+            }
+        ),
+    },
+    {
+        "path": "../docs/UGVs/sparrow3.md",
+        "items": ImageItems(
+            {
+                "https://github.com/kamangir/assets/blob/main/bluer-ugv/bluer-light.png?raw=true": ""
+            }
+        ),
+    },
+    {
+        "path": "../docs/UGVs/sparrow4.md",
+        "items": ImageItems(
+            {
+                "https://github.com/kamangir/assets/blob/main/bluer-ugv/bluer-light.png?raw=true": ""
+            }
+        ),
+    },
+]
