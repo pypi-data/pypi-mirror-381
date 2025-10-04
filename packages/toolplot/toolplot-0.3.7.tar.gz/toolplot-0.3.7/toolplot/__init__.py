5e29960892da3cdf4aa5f011bc085ed8812a0992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 07:05:01 +0000

    Add package toolplot/0.3.7/toolplot-0.3.7.tar.gz

---
+++ packages/toolplot/toolplot-0.3.7.tar.gz/toolplot-0.3.7/toolplot/__init__.py
@@ -0,0 +1,10 @@
+"""toolplot is a alternative API to plotting backends"""
+
+from .plot_create import *
+from .plot_save import *
+from .plot_setup import *
+from .plot_ticks import *
+from .special_plots import *
+
+
+__version__ = '0.3.7'
---
+++ packages/toolplot/toolplot-0.3.7.tar.gz/toolplot-0.3.7/toolplot/special_plots/__init__.py
@@ -0,0 +1,5 @@
+from .bar_plots import *
+from .candlestick_plots import *
+from .log_histograms import *
+from .two_dim_log_histogram import plot_2d_log_histogram
+from .group_plots import *
