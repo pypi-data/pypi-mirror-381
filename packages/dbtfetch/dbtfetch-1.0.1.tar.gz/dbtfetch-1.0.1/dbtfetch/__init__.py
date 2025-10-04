8a986ab7610e9982eaba0d5e0ae7605814b67fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 05:06:18 +0000

    Add package dbtfetch/1.0.1/dbtfetch-1.0.1.tar.gz

---
+++ packages/dbtfetch/dbtfetch-1.0.1.tar.gz/dbtfetch-1.0.1/dbtfetch/__init__.py
@@ -0,0 +1,9 @@
+"""dbtfetch - A neofetch-style system information tool for dbt projects."""
+
+__version__ = "1.0.1"
+__author__ = "Daniel Palma"
+__email__ = "danivgy@gmail.com"
+
+from dbtfetch.cli import main
+
+__all__ = ["main"]
