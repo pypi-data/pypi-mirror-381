79d1ec70f5fb7cdaf14344520f2cf873d1614aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 05:05:33 +0000

    Add package pgpack/0.3.0.1/pgpack-0.3.0.1.tar.gz

---
+++ packages/pgpack/pgpack-0.3.0.1.tar.gz/pgpack-0.3.0.1/pgpack/__init__.py
@@ -0,0 +1,29 @@
+"""Library for read and write storage format for PGCopy dump
+packed into LZ4, ZSTD or uncompressed
+with meta data information packed into zlib."""
+
+from light_compressor import CompressionMethod
+
+from .common import (
+    metadata_from_frame,
+    PGPackError,
+    PGPackHeaderError,
+    PGPackMetadataCrcError,
+    PGPackModeError,
+)
+from .reader import PGPackReader
+from .writer import PGPackWriter
+
+
+__all__ = (
+    "metadata_from_frame",
+    "CompressionMethod",
+    "PGPackError",
+    "PGPackHeaderError",
+    "PGPackMetadataCrcError",
+    "PGPackModeError",
+    "PGPackReader",
+    "PGPackWriter",
+)
+__author__ = "0xMihalich"
+__version__ = "0.3.0.1"
