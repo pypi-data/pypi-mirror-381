67d75dd94b7221e278a48a70bfe85175753cd6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 04:05:21 +0000

    Add package PyNiteFEA/1.5.0/pynitefea-1.5.0.tar.gz

---
+++ packages/PyNiteFEA/pynitefea-1.5.0.tar.gz/pynitefea-1.5.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyNiteFEA",
+    version="1.5.0",
+    author="D. Craig Brinck, PE, SE",
+    author_email="Building.Code@outlook.com",
+    description="A simple elastic 3D structural finite element library for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JWock82/Pynite.git",
+    packages=setuptools.find_packages(include=['Pynite', 'Pynite.*']),
+    package_data = {'Pynite': ['*html', '*.css', '*Full Logo No Buffer - Transparent.png']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'PrettyTable',
+        'scipy',
+        'matplotlib',
+    ],
+    extras_require = {
+        'all': ['Ipython', 'vtk', 'pyvista[all,trame]', 'trame_jupyter_extension', 'ipywidgets', 'pdfkit', 'Jinja2'],
+        'vtk':  ['IPython', 'vtk'],
+        'pyvista': ['pyvista[all,trame]', 'trame_jupyter_extension', 'ipywidgets'],
+        'reporting': ['pdfkit', 'Jinja2'],
+        'derivations': ['jupyterlab', 'sympy']
+    },
+    include_package_data = True,
+    python_requires = '>=3.7',
+)
