c51c42faa2302d8d895a4efcbc1bb97315c764f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 03:11:36 +0000

    Add package trilogy-public-models/0.1.17/trilogy_public_models-0.1.17-py3-none-any.whl

---
+++ packages/trilogy-public-models/trilogy_public_models-0.1.17-py3-none-any.whl/trilogy_public_models/inventory.py
@@ -0,0 +1,31 @@
+from os import listdir
+from os.path import dirname, join
+
+from trilogy import Environment
+from trilogy.constants import ENV_CACHE_NAME
+from trilogy.parser import parse
+from trilogy.authoring import RawSQLStatement, SelectStatement, PersistStatement
+
+
+def parse_initial_models(
+    fpath: str,
+) -> tuple[Environment, list[RawSQLStatement | SelectStatement | PersistStatement]]:
+    parent_folder = dirname(fpath)
+    files = listdir(dirname(fpath))
+    cache_path = join(parent_folder, ENV_CACHE_NAME)
+    # 2024-03-31 - disable due to issue with pydantic parsing from cache wrong
+    # for file in files:
+    #     if file == ENV_CACHE_NAME:
+    #         env = Environment.from_cache(join(parent_folder, file))
+    #         if env:
+    #             return env
+    for file in files:
+        if file.endswith("entrypoint.preql"):
+            with open(join(parent_folder, file), "r", encoding="utf-8") as f:
+                contents = f.read()
+                env = Environment(working_path=parent_folder)
+                environment, statements = parse(contents, environment=env)
+                env.to_cache(cache_path)
+                return environment, statements
+
+    raise ValueError(f"Missing entrypoint.preql in {fpath}")
