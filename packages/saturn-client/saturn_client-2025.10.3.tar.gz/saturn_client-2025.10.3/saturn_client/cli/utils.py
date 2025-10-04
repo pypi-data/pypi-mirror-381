e7672befe0547f4714a5c7bda79c82e42e378d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 07:04:57 +0000

    Add package saturn-client/2025.10.3/saturn_client-2025.10.3.tar.gz

---
+++ packages/saturn-client/saturn_client-2025.10.3.tar.gz/saturn_client-2025.10.3/saturn_client/cli/utils.py
@@ -0,0 +1,167 @@
+import json
+from dataclasses import asdict
+from enum import Enum
+import sys
+from os.path import splitext
+from typing import Any, Dict, List, Optional, Union
+
+import click
+from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import LiteralScalarString
+
+from saturn_client.run import Run
+
+
+class OutputFormat(str, Enum):
+    TABLE = "table"
+    JSON = "json"
+    YAML = "yaml"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    @classmethod
+    def values(cls) -> List[str]:
+        return [str(value) for value in list(cls)]  # type: ignore
+
+    @classmethod
+    def validate(cls, format: str):
+        if format not in cls.values():
+            raise ValueError(f'Unknown output format "{format}"')
+
+
+def print_json(data: Union[List, Dict]):
+    click.echo(json.dumps(data, indent=2))
+
+
+def maybe_sanitize_yaml(obj: Dict) -> Dict:
+    if "spec" in obj and "start_script" in obj["spec"]:
+        obj["spec"]["start_script"] = LiteralScalarString(obj["spec"]["start_script"])
+    return obj
+
+
+def print_resources(resource: Union[List, Dict], output: str = OutputFormat.TABLE):
+    output = output.lower()
+    OutputFormat.validate(output)
+    if output == OutputFormat.TABLE:
+        if not isinstance(resource, list):
+            resource = [resource]
+        print_resource_table(resource)
+    elif output == OutputFormat.YAML:
+        yaml = YAML()
+        yaml.default_flow_style = False
+        yaml.width = sys.maxsize
+        if isinstance(resource, list):
+            resource = [maybe_sanitize_yaml(x) for x in resource]
+            yaml.dump_all(resource, sys.stdout)
+        else:
+            resource = maybe_sanitize_yaml(resource)
+            yaml.dump(resource, sys.stdout)
+    else:
+        print_json(resource)
+
+
+def print_resource_table(
+    resources: List[Dict[str, Any]],
+):
+    headers = ["owner", "name", "resource_type", "status", "instance_type", "scale", "id"]
+    data: List[List[str]] = []
+    for resource in resources:
+        spec = resource["spec"]
+        state = resource.get("state", {})
+        id = state.get("id")
+        owner = spec.get("owner")
+        name = spec["name"]
+        resource_type = resource["type"]
+        status = state.get("status")
+        instance_type = spec["instance_type"]
+        scale = spec.get("scale", 1)
+        data.append([owner, name, resource_type, status, instance_type, scale, id])
+    tabulate(data, headers)
+
+
+def print_pod_table(
+    results: List[Dict[str, Any]],
+):
+    headers = ["pod_name", "status", "source", "start_time", "end_time"]
+    data: List[List[str]] = []
+    for pod in results:
+        pod_name = pod["pod_name"]
+        status = pod["status"]
+        source = pod["source"]
+        start_time = pod["start_time"] or ""
+        end_time = pod["end_time"] or ""
+        data.append([pod_name, status, source, start_time, end_time])
+    tabulate(data, headers)
+
+
+def tabulate(
+    data: List[List[Any]],
+    headers: List[str],
+    rpadding: int = 4,
+    justify: Optional[Dict[str, str]] = None,
+    minwidth: Optional[Dict[str, int]] = None,
+):
+    justify = justify if justify else {}
+    minwidth = minwidth if minwidth else {}
+    widths: List[int] = [0] * len(headers)
+    for i, header in enumerate(headers):
+        widths[i] = max(len(header), minwidth.get(header, 0))
+
+    for row in data:
+        for i, value in enumerate(row):
+            if value is None:
+                row[i] = value = ""
+            widths[i] = max(widths[i], len(str(value)))
+
+    header_format_str = ""
+    format_str = ""
+    for i, width in enumerate(widths):
+        justify_char = justify.get(headers[i], "<")
+        format_str += f"{{:{justify_char}{width}}}"
+        header_format_str += f"{{:<{width}}}"
+        if i < len(widths) - 1:
+            format_str += " " * rpadding
+            header_format_str += " " * rpadding
+
+    click.echo(header_format_str.format(*headers))
+    click.echo("-" * (sum(widths) + rpadding * (len(headers) - 1)))
+    for row in data:
+        click.echo(format_str.format(*row))
+
+
+def print_resource_op(
+    operation: str,
+    resource_type: str,
+    resource_name: Optional[str] = None,
+    owner_name: Optional[str] = None,
+    *args: str,
+):
+    parts = [
+        operation,
+        resource_type,
+        resource_name,
+    ]
+    if owner_name:
+        parts.append(f"for {owner_name}")
+    parts.extend(args)
+    click.echo(" ".join([p for p in parts if p]))
+
+
+def deserialize(path: str) -> Dict:
+    base, ext = splitext(path)
+    with open(path, "r") as f:
+        if ext in {".yml", ".yaml"}:
+            yaml = YAML()
+            return yaml.load(f)
+        return json.load(f)
+
+
+def print_run_status(runs: List[Run]):
+    headers = ["status", "status_code", "remote_output_path", "cmd"]
+    data = []
+    for r in runs:
+        d = asdict(r)
+        dd = [d[x] for x in headers]
+        data.append(dd)
+    tabulate(data, headers)
---
+++ packages/saturn-client/saturn_client-2025.10.3.tar.gz/saturn_client-2025.10.3/saturn_client/settings.py
@@ -0,0 +1,76 @@
+"""
+Settings used for interacting with Saturn
+"""
+
+import os
+
+from typing import Optional
+from urllib.parse import urlparse
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+
+
+class Settings:
+    """Global settings"""
+
+    BASE_URL: str
+    SATURN_TOKEN: str
+    REFRESH_TOKEN: Optional[str] = None
+    WORKING_DIRECTORY: str = "/home/jovyan/workspace"
+
+    def __init__(
+        self,
+        base_url: Optional[str] = None,
+        saturn_token: Optional[str] = None,
+        refresh_token: Optional[str] = None,
+    ):
+        if base_url:
+            self.BASE_URL = base_url
+        else:
+            try:
+                self.BASE_URL = os.environ["SATURN_BASE_URL"]
+            except KeyError as err:
+                err_msg = "Missing required value Saturn url."
+                raise RuntimeError(err_msg) from err
+
+        parsed = urlparse(self.BASE_URL)
+        if not parsed.scheme or not parsed.netloc:
+            raise ValueError(f'"{self.BASE_URL}" is not a valid URL')
+
+        if saturn_token:
+            self.SATURN_TOKEN = saturn_token
+        else:
+            try:
+                self.SATURN_TOKEN = os.environ["SATURN_TOKEN"]
+            except KeyError as err:
+                err_msg = "Missing required value Saturn api token."
+                raise RuntimeError(err_msg) from err
+
+        if refresh_token:
+            self.REFRESH_TOKEN = refresh_token
+        else:
+            self.REFRESH_TOKEN = os.getenv("SATURN_REFRESH_TOKEN")
+
+    def update_tokens(self, access_token: str, refresh_token: Optional[str] = None):
+        self.SATURN_TOKEN = access_token
+        if "SATURN_TOKEN" in os.environ:
+            os.environ["SATURN_TOKEN"] = access_token
+
+        self.REFRESH_TOKEN = refresh_token
+        if "SATURN_REFRESH_TOKEN" in os.environ:
+            os.environ["SATURN_REFRESH_TOKEN"] = refresh_token
+
+    @property
+    def url(self):
+        """Saturn url"""
+        return self.BASE_URL
+
+    @property
+    def headers(self):
+        """Saturn auth headers including saturn-client version"""
+        return {
+            "Authorization": f"token {self.SATURN_TOKEN}",
+            "X-Saturn-Client-Version": __version__,
+        }
