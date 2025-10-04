562ac504bef8be9c82e0d92c88d63e0214aa6566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 04:05:14 +0000

    Add package literate-octo-two-cu12/11.4.0/literate_octo_two_cu12-11.4.0-cp310-cp310-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/literate-octo-two-cu12/literate_octo_two_cu12-11.4.0-cp310-cp310-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/literate_octo_two_cu12/__platform__.py
@@ -0,0 +1,9 @@
+"""Platform-specific metadata for literate-octo-two-cu12."""
+PACKAGE_NAME = "literate-octo-two-cu12"
+VERSION = "11.4.0"
+PYTHON_VERSION = "cp310"
+ARCHITECTURE = "aarch64"
+PLATFORM_TAG = "manylinux_2_24_aarch64.manylinux_2_28_aarch64"
+
+# Unique build identifier to ensure different hash per wheel
+BUILD_ID = "1759502102760476544-literate-octo-two-cu12-cp310-aarch64"
