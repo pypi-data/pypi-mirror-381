3c3533975004b752fecf83da323ba94e804f6dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 05:06:09 +0000

    Add package freeze-core/0.2.1/freeze_core-0.2.1-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/freeze-core/freeze_core-0.2.1-cp311-cp311-manylinux_2_28_x86_64.whl/freeze_core/bases/__init__.py
@@ -0,0 +1,5 @@
+"""The source of the base executables which are used to launch your Python
+applications. Different bases serve for different types of application on
+Windows (GUI, console application or service). The base executable calls the
+initscript, which in turn calls the user's code.
+"""
