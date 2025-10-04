531c68c8d3da66654c0103c4cc09919e45c4828f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 05:06:39 +0000

    Add package sentry-kafka-management/0.0.5/sentry_kafka_management-0.0.5-py3-none-any.whl

---
+++ packages/sentry-kafka-management/sentry_kafka_management-0.0.5-py3-none-any.whl/sentry_kafka_management/cli.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+import click
+
+from sentry_kafka_management.scripts.brokers import describe_broker_configs
+from sentry_kafka_management.scripts.topics import list_topics
+
+COMMANDS = [describe_broker_configs, list_topics]
+
+
+@click.group()
+def main() -> None:
+    """
+    CLI entrypoint for sentry-kafka-management.
+    """
+    pass
+
+
+for command in COMMANDS:
+    main.add_command(command)
+
+if __name__ == "__main__":
+    main()
