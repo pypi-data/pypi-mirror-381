a8db2cdaefc851431973d35f01832a78ca4aa3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 06:05:18 +0000

    Add package py-ballisticcalc.exts/2.2.0/py_ballisticcalc_exts-2.2.0-pp39-pypy39_pp73-win_amd64.whl

---
+++ packages/py-ballisticcalc.exts/py_ballisticcalc_exts-2.2.0-pp39-pypy39_pp73-win_amd64.whl/py_ballisticcalc_exts/__init__.py
@@ -0,0 +1,19 @@
+__author__ = "o-murphy"
+__copyright__ = (
+    "Copyright 2023 Dmytro Yaroshenko (https://github.com/o-murphy)",
+    "Copyright 2024 David Bookstaber (https://github.com/dbookstaber)"
+)
+
+__credits__ = ["o-murphy", "dbookstaber"]
+
+from .euler_engine import CythonizedEulerIntegrationEngine
+from .rk4_engine import CythonizedRK4IntegrationEngine
+from .test_engine import CythonEngineTestHarness
+# from .trajectory_data import TrajectoryDataT
+
+__all__ = (
+    'CythonizedEulerIntegrationEngine',
+    'CythonizedRK4IntegrationEngine',
+    'CythonEngineTestHarness',
+    # 'TrajectoryDataT',
+)
