c843b5bc4a4537812088b795dbf040888106dede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 05:05:41 +0000

    Add package guarddog/2.7.0/guarddog-2.7.0.tar.gz

---
+++ packages/guarddog/guarddog-2.7.0.tar.gz/guarddog-2.7.0/guarddog/analyzer/metadata/pypi/typosquatting.py
@@ -0,0 +1,148 @@
+import json
+import logging
+import os
+from datetime import datetime, timedelta
+from typing import Optional
+
+import requests
+import packaging.utils
+
+from guarddog.analyzer.metadata.typosquatting import TyposquatDetector
+from guarddog.utils.config import TOP_PACKAGES_CACHE_LOCATION
+
+log = logging.getLogger("guarddog")
+
+
+class PypiTyposquatDetector(TyposquatDetector):
+    """
+    Detector for typosquatting attacks. Detects if a package name is a typosquat of one of the top 1000 packages.
+    Checks for distance one Levenshtein, one-off character swaps, permutations
+    around hyphens, and substrings.
+
+    Attributes:
+        popular_packages (list): list of top 5000 downloaded packages from PyPI
+    """
+
+    def _get_top_packages(self) -> set:
+        """
+        Gets the package information of the top 5000 most downloaded PyPI packages
+
+        Returns:
+            set: set of package data in the format:
+                {
+                    ...
+                    {
+                        download_count: ...
+                        project: <package-name>
+                    }
+                    ...
+                }
+        """
+
+        popular_packages_url = (
+            "https://hugovk.github.io/top-pypi-packages/top-pypi-packages.min.json"
+        )
+
+        top_packages_filename = "top_pypi_packages.json"
+        resources_dir = TOP_PACKAGES_CACHE_LOCATION
+        if resources_dir is None:
+            resources_dir = os.path.abspath(
+                os.path.join(os.path.dirname(__file__), "..", "resources")
+            )
+
+        top_packages_path = os.path.join(resources_dir, top_packages_filename)
+
+        top_packages_information = None
+
+        if top_packages_filename in os.listdir(resources_dir):
+            update_time = datetime.fromtimestamp(os.path.getmtime(top_packages_path))
+
+            if datetime.now() - update_time <= timedelta(days=30):
+                with open(top_packages_path, "r") as top_packages_file:
+                    top_packages_information = json.load(top_packages_file)["rows"]
+
+        if top_packages_information is None:
+            response = requests.get(popular_packages_url).json()
+            with open(top_packages_path, "w+") as f:
+                json.dump(response, f, ensure_ascii=False, indent=4)
+
+            top_packages_information = response["rows"]
+
+        def get_safe_name(package):
+            return packaging.utils.canonicalize_name(package["project"])
+
+        return set(map(get_safe_name, top_packages_information))
+
+    def detect(
+        self,
+        package_info,
+        path: Optional[str] = None,
+        name: Optional[str] = None,
+        version: Optional[str] = None,
+    ) -> tuple[bool, Optional[str]]:
+        """
+        Uses a package's information from PyPI's JSON API to determine the
+        package is attempting a typosquatting attack
+
+        Args:
+            package_info (dict): dictionary representation of PyPI's JSON
+                output
+
+        Returns:
+            list[str]: names of packages that <package_name> could be
+            typosquatting from
+            @param **kwargs:
+        """
+        log.debug(f"Running typosquatting heuristic on PyPI package {name}")
+        normalized_name = packaging.utils.canonicalize_name(
+            package_info["info"]["name"]
+        )
+        similar_package_names = self.get_typosquatted_package(normalized_name)
+        if len(similar_package_names) > 0:
+            return True, TyposquatDetector.MESSAGE_TEMPLATE % ", ".join(
+                similar_package_names
+            )
+        return False, None
+
+    def _get_confused_forms(self, package_name) -> list:
+        """
+        Gets confused terms for python packages
+        Confused terms are:
+            - py to python swaps (or vice versa)
+            - the removal of py/python terms
+
+        Args:
+            package_name (str): name of the package
+
+        Returns:
+            list: list of confused terms
+        """
+
+        confused_forms = []
+
+        terms = package_name.split("-")
+
+        # Detect swaps like python-package -> py-package
+        for i in range(len(terms)):
+            confused_term = None
+
+            if "python" in terms[i]:
+                confused_term = terms[i].replace("python", "py")
+            elif "py" in terms[i]:
+                confused_term = terms[i].replace("py", "python")
+            else:
+                continue
+
+            # Get form when replacing or removing py/python term
+            replaced_form = terms[:i] + [confused_term] + terms[i + 1 :]
+            removed_form = terms[:i] + terms[i + 1 :]
+
+            for form in (replaced_form, removed_form):
+                confused_forms.append("-".join(form))
+
+        return confused_forms
+
+
+if __name__ == "__main__":
+    # update top_pypi_packages.json
+    PypiTyposquatDetector()._get_top_packages()
---
+++ packages/guarddog/guarddog-2.7.0.tar.gz/guarddog-2.7.0/guarddog/utils/archives.py
@@ -0,0 +1,79 @@
+import logging
+import os
+import stat
+import zipfile
+
+import tarsafe  # type:ignore
+
+log = logging.getLogger("guarddog")
+
+
+def is_supported_archive(path: str) -> bool:
+    """
+    Decide whether a file contains a supported archive based on its
+    file extension.
+
+    Args:
+        path (str): The local filesystem path to examine
+
+    Returns:
+        bool: Represents the decision reached for the file
+
+    """
+
+    def is_tar_archive(path: str) -> bool:
+        tar_exts = [".bz2", ".bzip2", ".gz", ".gzip", ".tgz", ".xz"]
+
+        return any(path.endswith(ext) for ext in tar_exts)
+
+    def is_zip_archive(path: str) -> bool:
+        return any(path.endswith(ext) for ext in [".zip", ".whl", ".egg"])
+
+    return is_tar_archive(path) or is_zip_archive(path)
+
+
+def safe_extract(source_archive: str, target_directory: str) -> None:
+    """
+    safe_extract safely extracts archives to a target directory.
+
+    This function does not clean up the original archive and does not
+    create the target directory if it does not exist.  It also assumes
+    the source archive argument is a path to a regular file on the
+    local filesystem.
+
+    @param source_archive:      The archive to extract
+    @param target_directory:    The directory where to extract the archive to
+    @raise ValueError           If the archive type is unsupported
+
+    """
+    log.debug(f"Extracting archive {source_archive} to directory {target_directory}")
+    if tarsafe.is_tarfile(source_archive):
+
+        def add_exec(path):
+            st = os.stat(path)
+            os.chmod(path, st.st_mode | stat.S_IEXEC)
+
+        def add_read(path):
+            st = os.stat(path)
+            os.chmod(path, st.st_mode | stat.S_IREAD)
+
+        def recurse_add_perms(path):
+            add_exec(path)
+            for root, dirs, files in os.walk(path):
+                for d in dirs:
+                    add_exec(os.path.join(root, d))
+                for f in files:
+                    add_read(os.path.join(root, f))
+
+        tarsafe.open(source_archive).extractall(target_directory)
+        recurse_add_perms(target_directory)
+
+    elif zipfile.is_zipfile(source_archive):
+        with zipfile.ZipFile(source_archive, "r") as zip:
+            for file in zip.namelist():
+                # Note: zip.extract cleans up any malicious file name
+                # such as directory traversal attempts This is not the
+                # case of zipfile.extractall
+                zip.extract(file, path=os.path.join(target_directory, file))
+    else:
+        raise ValueError(f"unsupported archive extension: {source_archive}")
