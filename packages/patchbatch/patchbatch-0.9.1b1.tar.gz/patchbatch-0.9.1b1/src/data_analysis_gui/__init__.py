b58c211a98fe6227a2cfbab730adafa17aa16fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 04:05:23 +0000

    Add package patchbatch/0.9.1b1/patchbatch-0.9.1b1.tar.gz

---
+++ packages/patchbatch/patchbatch-0.9.1b1.tar.gz/patchbatch-0.9.1b1/src/data_analysis_gui/__init__.py
@@ -0,0 +1,10 @@
+"""
+PatchBatch Electrophysiology Data Analysis Tool
+Author: Charles Kissell, Northeastern University
+License: MIT (see LICENSE file for details)
+"""
+
+__version__ = "0.9.1-beta.1"
+
+# Import core module to make it accessible
+from . import core
