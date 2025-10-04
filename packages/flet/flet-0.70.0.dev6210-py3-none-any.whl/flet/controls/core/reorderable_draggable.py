0ebc951246ed65a685ad2e88c6ad5cbd931ad3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 05:05:27 +0000

    Add package flet/0.70.0.dev6210/flet-0.70.0.dev6210-py3-none-any.whl

---
+++ packages/flet/flet-0.70.0.dev6210-py3-none-any.whl/flet/controls/core/reorderable_draggable.py
@@ -0,0 +1,33 @@
+from flet.controls.adaptive_control import AdaptiveControl
+from flet.controls.base_control import control
+from flet.controls.control import Control
+from flet.controls.layout_control import LayoutControl
+
+
+@control("ReorderableDraggable")
+class ReorderableDraggable(LayoutControl, AdaptiveControl):
+    """
+    Used to drag an item in a [`ReorderableListView`][flet.].
+
+    It creates a listener for a drag immediately following a pointer down
+    event over the given [`content`][(c).] control.
+
+    Raises:
+        ValueError: If [`content`][(c).] is not visible.
+    """
+
+    index: int
+    """
+    The index of the associated item that will be dragged in the list.
+    """
+
+    content: Control
+    """
+    The control for which the application would like to respond to a tap and
+    drag gesture by starting a reordering drag on a reorderable list.
+    """
+
+    def before_update(self):
+        super().before_update()
+        if not self.content.visible:
+            raise ValueError("content must be visible")
