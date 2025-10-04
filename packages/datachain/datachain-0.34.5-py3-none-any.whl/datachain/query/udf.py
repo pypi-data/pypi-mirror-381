d2249e42200fc8349297087028ed490fecdd8f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 14:05:57 +0000

    Add package datachain/0.34.5/datachain-0.34.5-py3-none-any.whl

---
+++ packages/datachain/datachain-0.34.5-py3-none-any.whl/datachain/query/udf.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any, Callable, Optional, TypedDict, Union
+
+if TYPE_CHECKING:
+    from sqlalchemy import Select, Table
+
+    from datachain.catalog import Catalog
+    from datachain.query.batch import BatchingStrategy
+
+
+class UdfInfo(TypedDict):
+    udf_data: bytes
+    catalog_init: dict[str, Any]
+    metastore_clone_params: tuple[Callable[..., Any], list[Any], dict[str, Any]]
+    warehouse_clone_params: tuple[Callable[..., Any], list[Any], dict[str, Any]]
+    table: "Table"
+    query: "Select"
+    udf_fields: list[str]
+    batching: "BatchingStrategy"
+    processes: Optional[int]
+    is_generator: bool
+    cache: bool
+    rows_total: int
+    batch_size: int
+
+
+class AbstractUDFDistributor(ABC):
+    @abstractmethod
+    def __init__(
+        self,
+        catalog: "Catalog",
+        table: "Table",
+        query: "Select",
+        udf_data: bytes,
+        batching: "BatchingStrategy",
+        workers: Union[bool, int],
+        processes: Union[bool, int],
+        udf_fields: list[str],
+        rows_total: int,
+        use_cache: bool,
+        is_generator: bool = False,
+        min_task_size: Optional[Union[str, int]] = None,
+        batch_size: Optional[int] = None,
+    ) -> None: ...
+
+    @abstractmethod
+    def __call__(self) -> None: ...
+
+    @staticmethod
+    @abstractmethod
+    def run_udf(fd: Optional[int] = None) -> int: ...
