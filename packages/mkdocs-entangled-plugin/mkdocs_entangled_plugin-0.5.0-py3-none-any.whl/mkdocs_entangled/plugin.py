29e701b9a60b17a2bacc22fe948cbb91c1fd1658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 04:05:06 +0000

    Add package mkdocs-entangled-plugin/0.5.0/mkdocs_entangled_plugin-0.5.0-py3-none-any.whl

---
+++ packages/mkdocs-entangled-plugin/mkdocs_entangled_plugin-0.5.0-py3-none-any.whl/mkdocs_entangled/plugin.py
@@ -0,0 +1,14 @@
+from typing import override
+from mkdocs.plugins import BasePlugin
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.structure.pages import Page
+from mkdocs.structure.files import Files
+
+from .on_page_markdown import on_page_markdown
+from .config import EntangledConfig
+
+
+class EntangledPlugin(BasePlugin[EntangledConfig]):
+    @override
+    def on_page_markdown(self, markdown: str, *, page: Page, config: MkDocsConfig, files: Files):
+        return on_page_markdown(markdown, page=page, config=config, files=files)
