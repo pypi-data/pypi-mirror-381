ccd4f15cc358d091159313ccbc5ab4c8ce72d22d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 06:06:07 +0000

    Add package perceptual-map-closures/2.0.3/perceptual_map_closures-2.0.3-cp313-cp313-win_amd64.whl

---
+++ packages/perceptual-map-closures/perceptual_map_closures-2.0.3-cp313-cp313-win_amd64.whl/map_closures/pybind/x64/vc17/staticlib/OpenCVModules-release.cmake
@@ -0,0 +1,139 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "zlib" for configuration "Release"
+set_property(TARGET zlib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(zlib PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/zlib.lib"
+  )
+
+list(APPEND _cmake_import_check_targets zlib )
+list(APPEND _cmake_import_check_files_for_zlib "${_IMPORT_PREFIX}/x64/vc17/staticlib/zlib.lib" )
+
+# Import target "libjpeg-turbo" for configuration "Release"
+set_property(TARGET libjpeg-turbo APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libjpeg-turbo PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/libjpeg-turbo.lib"
+  )
+
+list(APPEND _cmake_import_check_targets libjpeg-turbo )
+list(APPEND _cmake_import_check_files_for_libjpeg-turbo "${_IMPORT_PREFIX}/x64/vc17/staticlib/libjpeg-turbo.lib" )
+
+# Import target "libtiff" for configuration "Release"
+set_property(TARGET libtiff APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libtiff PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C;CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/libtiff.lib"
+  )
+
+list(APPEND _cmake_import_check_targets libtiff )
+list(APPEND _cmake_import_check_files_for_libtiff "${_IMPORT_PREFIX}/x64/vc17/staticlib/libtiff.lib" )
+
+# Import target "libwebp" for configuration "Release"
+set_property(TARGET libwebp APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libwebp PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/libwebp.lib"
+  )
+
+list(APPEND _cmake_import_check_targets libwebp )
+list(APPEND _cmake_import_check_files_for_libwebp "${_IMPORT_PREFIX}/x64/vc17/staticlib/libwebp.lib" )
+
+# Import target "libopenjp2" for configuration "Release"
+set_property(TARGET libopenjp2 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libopenjp2 PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/libopenjp2.lib"
+  )
+
+list(APPEND _cmake_import_check_targets libopenjp2 )
+list(APPEND _cmake_import_check_files_for_libopenjp2 "${_IMPORT_PREFIX}/x64/vc17/staticlib/libopenjp2.lib" )
+
+# Import target "libpng" for configuration "Release"
+set_property(TARGET libpng APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libpng PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/libpng.lib"
+  )
+
+list(APPEND _cmake_import_check_targets libpng )
+list(APPEND _cmake_import_check_files_for_libpng "${_IMPORT_PREFIX}/x64/vc17/staticlib/libpng.lib" )
+
+# Import target "IlmImf" for configuration "Release"
+set_property(TARGET IlmImf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(IlmImf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/IlmImf.lib"
+  )
+
+list(APPEND _cmake_import_check_targets IlmImf )
+list(APPEND _cmake_import_check_files_for_IlmImf "${_IMPORT_PREFIX}/x64/vc17/staticlib/IlmImf.lib" )
+
+# Import target "ippiw" for configuration "Release"
+set_property(TARGET ippiw APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ippiw PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/ippiw.lib"
+  )
+
+list(APPEND _cmake_import_check_targets ippiw )
+list(APPEND _cmake_import_check_files_for_ippiw "${_IMPORT_PREFIX}/x64/vc17/staticlib/ippiw.lib" )
+
+# Import target "ittnotify" for configuration "Release"
+set_property(TARGET ittnotify APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ittnotify PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/ittnotify.lib"
+  )
+
+list(APPEND _cmake_import_check_targets ittnotify )
+list(APPEND _cmake_import_check_files_for_ittnotify "${_IMPORT_PREFIX}/x64/vc17/staticlib/ittnotify.lib" )
+
+# Import target "ade" for configuration "Release"
+set_property(TARGET ade APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ade PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/ade.lib"
+  )
+
+list(APPEND _cmake_import_check_targets ade )
+list(APPEND _cmake_import_check_files_for_ade "${_IMPORT_PREFIX}/x64/vc17/staticlib/ade.lib" )
+
+# Import target "opencv_core" for configuration "Release"
+set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_core PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/opencv_core4110.lib"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_core )
+list(APPEND _cmake_import_check_files_for_opencv_core "${_IMPORT_PREFIX}/x64/vc17/staticlib/opencv_core4110.lib" )
+
+# Import target "opencv_imgproc" for configuration "Release"
+set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_imgproc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/opencv_imgproc4110.lib"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgproc )
+list(APPEND _cmake_import_check_files_for_opencv_imgproc "${_IMPORT_PREFIX}/x64/vc17/staticlib/opencv_imgproc4110.lib" )
+
+# Import target "opencv_features2d" for configuration "Release"
+set_property(TARGET opencv_features2d APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_features2d PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc17/staticlib/opencv_features2d4110.lib"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_features2d )
+list(APPEND _cmake_import_check_files_for_opencv_features2d "${_IMPORT_PREFIX}/x64/vc17/staticlib/opencv_features2d4110.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
