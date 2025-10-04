b5fab8848b39689a5d0d30686ab512cda79f3d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 04:05:35 +0000

    Add package profiplots/3.0.3/profiplots-3.0.3-py3-none-any.whl

---
+++ packages/profiplots/profiplots-3.0.3-py3-none-any.whl/profiplots/style/__init__.py
@@ -0,0 +1,21 @@
+"""
+This package provides us with various styles. We can use them to modify the default theme look.
+These styles include:
+
+- `grey`: sets greyscale as the default color palette.
+- `colored`: sets colored palette as the default.
+- `grid`: adds grids to the plots.
+- ...
+"""
+
+from profiplots.style._modifier import bluescale, colored, grey, greyscale, grid, spines, ticks
+
+__all__ = [
+    "grey",
+    "greyscale",
+    "bluescale",
+    "colored",
+    "grid",
+    "spines",
+    "ticks"
+]
