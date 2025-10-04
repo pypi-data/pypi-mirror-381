4298c0c44b6d2678d15d021e394818ffce399b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 15:06:11 +0000

    Add package fovus/2.0.31/fovus-2.0.31-py3-none-any.whl

---
+++ packages/fovus/fovus-2.0.31-py3-none-any.whl/fovus/commands/pipeline/pipeline_command.py
@@ -0,0 +1,17 @@
+import click
+
+from fovus.commands.pipeline.commands.create.pipeline_create_command import (
+    pipeline_create_command,
+)
+from fovus.commands.pipeline.commands.update.pipeline_update_command import (
+    pipeline_update_command,
+)
+
+
+@click.group("pipeline")
+def pipeline_command():
+    """Contains commands related to pipeline."""
+
+
+pipeline_command.add_command(pipeline_create_command)
+pipeline_command.add_command(pipeline_update_command)
