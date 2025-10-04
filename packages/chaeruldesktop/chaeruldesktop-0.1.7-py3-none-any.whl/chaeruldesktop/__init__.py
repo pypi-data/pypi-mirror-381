0e8dc4fd24a23c163751a9a69f2153c54ba6f6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 03:11:17 +0000

    Add package chaeruldesktop/0.1.7/chaeruldesktop-0.1.7-py3-none-any.whl

---
+++ packages/chaeruldesktop/chaeruldesktop-0.1.7-py3-none-any.whl/chaeruldesktop/__init__.py
@@ -0,0 +1,12 @@
+from .app_desktop import (
+    inputView,
+    textView,
+    buttonView,
+    appDesktop,
+    config,
+    ImageViewPilLow,
+    styleInput,
+    progressView,
+    safe_callback,
+    Colors
+)
