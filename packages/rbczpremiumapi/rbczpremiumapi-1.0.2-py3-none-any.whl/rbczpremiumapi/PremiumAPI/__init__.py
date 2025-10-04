bed65a76fa487c50e1eda1be93c1067de7b7c04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-04 04:05:04 +0000

    Add package rbczpremiumapi/1.0.2/rbczpremiumapi-1.0.2-py3-none-any.whl

---
+++ packages/rbczpremiumapi/rbczpremiumapi-1.0.2-py3-none-any.whl/rbczpremiumapi/PremiumAPI/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from .download_statement_api import DownloadStatementApi
+from .get_account_balance_api import GetAccountBalanceApi
+from .get_accounts_api import GetAccountsApi
+from .get_batch_detail_api import GetBatchDetailApi
+from .get_fx_rates_api import GetFxRatesApi
+from .get_fx_rates_list_api import GetFxRatesListApi
+from .get_statement_list_api import GetStatementListApi
+from .get_transaction_list_api import GetTransactionListApi
+from .upload_payments_api import UploadPaymentsApi
+
